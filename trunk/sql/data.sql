d61635c101436c5294de8bb708b1599057061cda
ethan <ethan@befef031-aaeb-48d0-b70f-681328309d3d>
2011-03-03 16:09:39 +0000

    new sql
    git-svn-id: http://svn.localprojects.net/gam2@10 befef031-aaeb-48d0-b70f-681328309d3d

---
+++ trunk/sql/data.sql
@@ -0,0 +1,8 @@
+# DATA
+insert into user_group (user_group_id, group_name, description) values (1, 'admin', 'Site admin/super users');
+insert into user_group (user_group_id, group_name, description) values (2, 'moderator', 'Site moderators');
+insert into user_group (user_group_id, group_name, description) values (3, 'leader', 'Reponse leaders/"endorsers"');
+
+insert into badwords (kill_words) values
+('shit fuck twat cunt blowjob buttplug dildo felching fudgepacker jizz smegma clitoris asshole bullshit bullshitter bullshitters bullshitting chickenshit chickenshits clit cockhead cocksuck cocksucker cocksucking cum cumming cums cunt cuntree cuntry cunts dipshit dipshits dumbfuck dumbfucks dumbshit dumbshits fuck fucka fucke fucked fucken fucker fuckers fuckface fuckhead fuckheads fuckhed fuckin fucking fucks fuckup fuckups kunt kuntree kuntry kunts motherfuck motherfucken motherfucker motherfuckers motherfuckin motherfucking shit shitface shitfaced shithead shitheads shithed shits shitting shitty');
+
