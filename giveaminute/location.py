e9df5ac425b09fa70ca36acd13267a2279b936d5
Ethan Holda <ethan@localprojects.net>
2011-03-08 19:42:54 +0000

    fixed dynamic scores for map
    git-svn-id: http://svn.localprojects.net/gam2/trunk@84 befef031-aaeb-48d0-b70f-681328309d3d

--- giveaminute/location.py
+++ giveaminute/location.py
@@ -5,14 +5,14 @@ def getLocationDictionary(db):
         # TODO
         # this is temporary until actual scoring is determined
         sql = """select l.location_id, l.name, l.lat, l.lon, t.score from location l
-                inner join temp_scores t on t.location_id = l.location_id
-                order by location_id""";
+                left join temp_scores t on t.location_id = l.location_id
+                order by l.location_id""";
         data = list(db.query(sql))
         
         locations = {}
         
         for item in data:
-            locations[item.name] = {'location_id':item.location_id, 'lat':str(item.lat), 'lon':str(item.lon)}
+            locations[item.name] = {'location_id':item.location_id, 'lat':str(item.lat), 'lon':str(item.lon), 'score': (item.score if item.score else 0)}
             
         return locations
     except Exception, e:
