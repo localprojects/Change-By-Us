1e6717adac11f225381e475829d324fa113dd1db
ethan <ethan@befef031-aaeb-48d0-b70f-681328309d3d>
2011-03-08 23:42:00 +0000

    project create services
    git-svn-id: http://svn.localprojects.net/gam2@96 befef031-aaeb-48d0-b70f-681328309d3d

--- trunk/controllers/createProject.py
+++ trunk/controllers/createProject.py
@@ -1,40 +1,86 @@
 import giveaminute.keywords as keywords
 import giveaminute.project as project
+import giveaminute.projectResource as resource
 from framework.controller import *
+import lib.web
 
 class CreateProject(Controller):
     def GET(self, action=None):
         if (action == 'keywords'):
-            s = "%s %s" % (self.request('text'), self.request('title'))
-            kw = keywords.getKeywords(self.db, s)
-            
-            log.info(kw)
-            
-            dic = dict(suggested_keywords=kw)
-        
-            return self.json(dic)
+            return self.getKeywordsJSON()
+        elif (action == 'similar'):
+            return self.getSimilarProjectsJSON()
+        elif (action == 'resources'):
+            return self.getSimilarResourcesJSON()
         else:
             return self.render('create')
             
-    def POST(self):
-        return self.newProject()
-        
-        
+    def POST(self,*args, **kw):
+        return self.newProject()  
         
     def newProject(self):
         title = self.request('title')
         description = self.request('text')
-        keywords = ' '.join(self.request('keywords'))
         locationId = self.request('location')
         imageId = self.request('image')
-        resourceIds = self.request('resources')
+        keywords = self.request('keywords').split(',')
+        resourceIds = self.request('resources').split(',')
         
-        projectId = project.createProject(self.db, title, description, keywords, locationId, imageId)
+        projectId = project.createProject(self.db, title, description, ' '.join(keywords), locationId, imageId)
+        
+        ##TODO add keywords to dictionary, hence the splitting and joining
         
         for resourceId in resourceIds:
+            log.info("*** insert resource id %s" % resourceId)
             project.attachResourceToProject(self.db, projectId, resourceId)
             
         if (projectId):
             return True
         else:
             return False
+            
+    def getKeywordsJSON(self):
+        s = "%s %s" % (self.request('text'), self.request('title'))
+        kw = keywords.getKeywords(self.db, s)
+        
+        log.info(kw)
+        
+        obj = dict(suggested_keywords=kw)
+    
+        return self.json(obj)    
+        
+    def getSimilarProjectsJSON(self):
+        locationId = self.request('location')
+        keywords = self.request('keywords').split(',') if self.request('keywords') else []
+                
+        if (locationId and len(keywords) > 0):
+            projects = project.getProjects(self.db, keywords, locationId)
+        elif (locationId):
+            projects = project.getProjectsByLocation(self.db, locationId)
+        elif (len(keywords) > 0):
+            projects = project.getProjectsByKeywords(self.db, keywords)
+        else:
+            return None
+        
+        obj = dict(projects = projects)
+        
+        return self.json(obj)
+        
+    def getSimilarResourcesJSON(self):
+        locationId = self.request('location')
+        keywords = self.request('keywords').split(',') if self.request('keywords') else []
+                
+        if (locationId and len(keywords) > 0):
+            resources = resource.getProjectResources(self.db, keywords, locationId)
+        elif (locationId):
+            resources = resource.getProjectResourcesByLocation(self.db, locationId)
+        elif (len(keywords) > 0):
+            resources = resource.getProjectResourcesByKeywords(self.db, keywords)
+        else:
+            return None
+        
+        obj = dict(resources = resources)
+        
+        return self.json(obj)
+    
+    
