36a2a3a3958abae2248cda7d916904197cb04785
ethan <ethan@befef031-aaeb-48d0-b70f-681328309d3d>
2011-03-08 16:21:39 +0000

    unique email and keyword
    git-svn-id: http://svn.localprojects.net/gam2@69 befef031-aaeb-48d0-b70f-681328309d3d

--- trunk/sql/data.sql
+++ trunk/sql/data.sql
@@ -6,3 +6,13 @@ insert into user_group (user_group_id, group_name, description) values (3, 'lead
 insert into badwords (kill_words) values
 ('shit fuck twat cunt blowjob buttplug dildo felching fudgepacker jizz smegma clitoris asshole bullshit bullshitter bullshitters bullshitting chickenshit chickenshits clit cockhead cocksuck cocksucker cocksucking cum cumming cums cunt cuntree cuntry cunts dipshit dipshits dumbfuck dumbfucks dumbshit dumbshits fuck fucka fucke fucked fucken fucker fuckers fuckface fuckhead fuckheads fuckhed fuckin fucking fucks fuckup fuckups kunt kuntree kuntry kunts motherfuck motherfucken motherfucker motherfuckers motherfuckin motherfucking shit shitface shitfaced shithead shitheads shithed shits shitting shitty');
 
+insert into keyword (keyword) values ('green');
+insert into keyword (keyword) values ('neighborhood');
+insert into keyword (keyword) values ('garden');
+insert into keyword (keyword) values ('water');
+insert into keyword (keyword) values ('plants');
+insert into keyword (keyword) values ('bicycle');
+insert into keyword (keyword) values ('cars');
+insert into keyword (keyword) values ('flowers');
+insert into keyword (keyword) values ('trees');
+
