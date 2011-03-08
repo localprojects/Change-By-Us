d29a5cd2db72da09a6cc9ca42cc266e7874f0cad
amahon <amahon@befef031-aaeb-48d0-b70f-681328309d3d>
2011-03-08 04:00:27 +0000

    workin on location drop
    git-svn-id: http://svn.localprojects.net/gam2@48 befef031-aaeb-48d0-b70f-681328309d3d

--- trunk/static/js/tc.gam.locationDropdown.js
+++ trunk/static/js/tc.gam.locationDropdown.js
@@ -21,10 +21,12 @@ tc.locationDropdown.prototype.init = function(options){
 
 tc.locationDropdown.prototype.bindEvents = function(){
 	tc.util.log('tc.locationDropdown.bindEvents');
-	this.options.input.bind('focus blur keyup',{dropdown:this},function(e){
+	this.options.input.bind('focus blur keydown keyup keypress',{dropdown:this},function(e){
 		switch(e.type){
 			case 'focus':e.data.dropdown.inputFocusHandler(e);break;
 			case 'keyup':e.data.dropdown.inputKeyUpHandler(e);break;
+			case 'keydown':e.data.dropdown.inputKeyDownHandler(e);break;
+			case 'keypress':e.data.dropdown.inputKeyPressHandler(e);break;
 			case 'blur':break;
 		}
 	});
@@ -52,8 +54,60 @@ tc.locationDropdown.prototype.inputFocusHandler = function(e){
 }
 
 tc.locationDropdown.prototype.inputKeyUpHandler = function(e){
-	tc.util.log('tc.locationDropdown.keyUpHandler');
+	tc.util.log('tc.locationDropdown.inputKeyUpHandler');
+	if(e.which == 38 || e.which == 40 || e.which == 13){
+		e.preventDefault();
+		e.stopPropagation();
+		return;
+	}
 	this.superFilterAndUpdateList(e.target.value);
+}
+
+tc.locationDropdown.prototype.inputKeyDownHandler = function(e){
+	tc.util.log('tc.locationDropdown.inputKeyDownHandler');
+	switch(e.which){
+		case 13:
+			e.preventDefault();
+			e.stopPropagation();
+			this.modifySelection(0);
+			break;
+	  case 38:
+			e.preventDefault();
+			this.modifySelection(-1);
+			break;
+		case 40:
+			e.preventDefault();
+			this.modifySelection(1);
+			break;
+	}
+}
+
+tc.locationDropdown.prototype.inputKeyPressHandler = function(e){
+	tc.util.log('tc.locationDropdown.inputKeyPressHandler');
+	if(e.which == 13){
+		e.preventDefault();
+	}
+}
+
+tc.locationDropdown.prototype.modifySelection = function(direction){
+	tc.util.log('tc.locationDropdown.modifySelection');
+	var currently_selected;
+	currently_selected = this.options.list.find('.selected');
+	if(!currently_selected.length){
+		this.options.list.find('li:first').addClass('selected');
+		return;
+	}
+	switch(direction){
+		case -1:
+			currently_selected.removeClass('selected').prev().addClass('selected');
+			break;
+		case 0:
+			currently_selected.children('a').click();
+			break;
+		case 1:
+			currently_selected.removeClass('selected').next().addClass('selected');
+			break;
+	}
 }
 
 tc.locationDropdown.prototype.radioHandler = function(e){
@@ -74,8 +128,6 @@ tc.locationDropdown.prototype.radioHandler = function(e){
 			}
 			break;
 	}
-	
-	tc.util.dump(e.target.id);
 }
 
 tc.locationDropdown.prototype.superFilterAndUpdateList = function(text,skipUpdate){
@@ -111,14 +163,12 @@ tc.locationDropdown.prototype.superFilterAndUpdateList = function(text,skipUpdat
 		this.options.warning.show();
 		this.options.input.removeClass('valid').addClass('not-valid');
 	} else {
-		//if(n_filtered > 20){
-		//	this.options.list.hide();
-		//}
+		this.options.list.children('ul').get(0).innerHTML = html;
 		if(n_filtered == 1){
 			this.options.input.removeClass('not-valid').addClass('valid').attr('valid-location','true');
+			this.options.list.find('li:first').addClass('selected');
 		} else {
 			this.options.list.show();
 		}
-		this.options.list.children('ul').get(0).innerHTML = html;
 	} 
 }
