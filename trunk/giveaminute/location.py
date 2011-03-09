ba21bdaf720fefa49a7d2a4a0c58b2f9d981ae6c
ethan <ethan@befef031-aaeb-48d0-b70f-681328309d3d>
2011-03-09 23:08:32 +0000

    better location control
    git-svn-id: http://svn.localprojects.net/gam2@127 befef031-aaeb-48d0-b70f-681328309d3d

--- trunk/giveaminute/location.py
+++ trunk/giveaminute/location.py
@@ -1,6 +1,8 @@
 from framework.log import log
 
 def getLocationDictionary(db):
+    locations = {}
+
     try:
         # TODO
         # this is temporary until actual scoring is determined
@@ -9,13 +11,51 @@ def getLocationDictionary(db):
                 order by l.location_id""";
         data = list(db.query(sql))
         
-        locations = {}
-        
         for item in data:
             locations[item.name] = {'location_id':item.location_id, 'lat':str(item.lat), 'lon':str(item.lon), 'score': (item.score if item.score else 0)}
-            
-        return locations
     except Exception, e:
         log.info("*** couldn't get or write locations")
         log.error(e)
-        return None
+
+    return locations
+  
+def getLocationsWithScoring(db):
+    data = []
+
+    try:
+        # TODO
+        # this is temporary until actual scoring is determined
+        sql = """select l.location_id, l.name, l.lat, l.lon, t.score from location l
+                left join temp_scores t on t.location_id = l.location_id
+                order by l.location_id""";
+        data = list(db.query(sql))
+    except Exception, e:
+        log.info("*** couldn't get locations")
+        log.error(e)
+
+    return data 
+        
+def getLocations(db):
+    data = []
+
+    try:
+        sql = """select l.location_id, l.name, l.lat, l.lon from location l
+                order by l.location_id""";
+        data = list(db.query(sql))
+    except Exception, e:
+        log.info("*** couldn't get locations")
+        log.error(e)
+
+    return data 
+
+def getSimpleLocationDictionary(db):
+    data = getLocations(db)
+    
+    locations = []
+    
+    for item in data:
+        locations.append({'name':item.name, 'location_id':item.location_id})
+        
+    return locations
+    
+    
