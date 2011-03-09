8bc814b0cac823d207fb13be3ddf351cb79380c5
ethan <ethan@befef031-aaeb-48d0-b70f-681328309d3d>
2011-03-09 23:09:21 +0000

    better location control
    git-svn-id: http://svn.localprojects.net/gam2@128 befef031-aaeb-48d0-b70f-681328309d3d

--- trunk/controllers/search.py
+++ trunk/controllers/search.py
@@ -1,20 +1,23 @@
 import framework.util as util
-import giveaminute.location as location
+import giveaminute.location as mLocation
 from framework.controller import *
 
 class Search(Controller):
     def GET(self, action=None):
         if action == 'map':
-            neighborhoodJSON = self.json(location.getLocationDictionary(self.db))
-            
-            return self.render('map', {'neighborhoodJSON':neighborhoodJSON})
+            return self.render('map', {'neighborhoodJSON':self.getLocationJSON()})
         else:
             log.info("*** render search")
             return self.render('search')   
             
+    def getLocationJSON(self):
+        data = mLocation.getLocationsWithScoring(self.db)
+        
+        locations = {}
+        
+        for item in data:
+            locations[item.name] = {'location_id':item.location_id, 'lat':str(item.lat), 'lon':str(item.lon), 'score': (item.score if item.score else 0)}
             
-            
-
-            
+        return self.json(dict(locations))            
             
             
