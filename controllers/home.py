a7640557465f7d72c5931f3b2b9def72af814914
Ethan Holda <ethan@localprojects.net>
2011-03-10 18:14:34 +0000

    dummy user data and template_data variable
    git-svn-id: http://svn.localprojects.net/gam2/trunk@152 befef031-aaeb-48d0-b70f-681328309d3d

--- controllers/home.py
+++ controllers/home.py
@@ -1,11 +1,18 @@
 from framework.controller import *
 import giveaminute.location as mLocation
+import giveaminute.user as mUser
 
 class Home(Controller):
     def GET(self, action=None):
+        self.template_data['user'] = dict(object = self.json(mUser.getDummyDictionary()),
+                                            is_admin = True,
+                                            is_moderator = True,
+                                            is_leader = True)
         if (action and action != 'home'):
             return self.render(action)
         else:
             locations = self.json(mLocation.getSimpleLocationDictionary(self.db))
-        
+            
+            self.template_data['locations'] = locations
+            
             return self.render('home',  {'locations':locations})
