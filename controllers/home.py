4c041379b20b9ed966616ffac922f885a4381989
Ethan Holda <ethan@localprojects.net>
2011-03-10 20:44:01 +0000

    stubbed out project user data and login/logout
    git-svn-id: http://svn.localprojects.net/gam2/trunk@159 befef031-aaeb-48d0-b70f-681328309d3d

--- controllers/home.py
+++ controllers/home.py
@@ -8,11 +8,58 @@ class Home(Controller):
                                             is_admin = True,
                                             is_moderator = True,
                                             is_leader = True)
+        self.template_data['project_user'] = dict(is_member = True,
+                                                is_project_admin = True)                                            
+                                            
         if (action and action != 'home'):
             return self.render(action)
         else:
-            locations = self.json(mLocation.getSimpleLocationDictionary(self.db))
+            return self.showHome()
             
-            self.template_data['locations'] = locations
+    def POST(self, action=None):
+        if (action == 'login'):
+            email = self.request('email')
+            password = self.request('password')
             
-            return self.render('home',  {'locations':locations})
+            if (email and password):
+                return True
+            else:
+                return False
+         
+            return True
+        elif (action == 'logout'):
+            return True
+        else:
+            return self.not_found()
+            
+            
+    def showHome(self):
+        locations = self.json(mLocation.getSimpleLocationDictionary(self.db))
+        
+        self.template_data['locations'] = locations
+        self.template_data['all_ideas'] = self.json(self.getAllProjectIdeas())
+        
+        return self.render('home',  {'locations':locations})    
+        
+    def getAllProjectIdeas(self):
+        data = []
+        
+        data.append(self.getProjectIdeas(1, 50))
+        data.append(self.getProjectIdeas(2, 30))
+        data.append(self.getProjectIdeas(3, 30))
+        data.append(self.getProjectIdeas(4, 30))
+        data.append(self.getProjectIdeas(5, 15))
+        
+        return data
+        
+    def getProjectIdeas(self, index, num):
+        data = []
+        
+        for i in range(num):
+            data.append(dict(text = "More Trees in New York City would be awesome and it would totally help to make it way more green! Let's plant some!",
+                            f_name = "John",
+                            l_name = "Smith",
+                            submitted_by = "web"))
+            
+        return data
+        
