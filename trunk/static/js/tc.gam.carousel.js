cd7a13c77b4427fbde8742b6840322051366c9cc
lkanter <lkanter@befef031-aaeb-48d0-b70f-681328309d3d>
2011-03-12 01:44:56 +0000

    some more work on carousels
    git-svn-id: http://svn.localprojects.net/gam2@190 befef031-aaeb-48d0-b70f-681328309d3d

--- trunk/static/js/tc.gam.carousel.js
+++ trunk/static/js/tc.gam.carousel.js
@@ -3,17 +3,36 @@ if (!tc) { var tc = {}; }
 tc.carousel = makeClass();
 
 tc.carousel.prototype.options = {
-	element: null
+	element: null,
+	scrollPaneSelector: ".scrollable:first",
+	itemsSelector: ".items"
 };
 
 tc.carousel.prototype.carousel = null;
 
 tc.carousel.prototype.init = function(app, options) {
+	var scrollPane, w, h;
 	tc.util.log("tc.carousel.init");
 	this.options = tc.jQ.extend(this.options, options);
-	this.options.element.scrollable({
+	
+	scrollPane = this.options.element.find(this.options.scrollPaneSelector);
+	w = this.options.element.width();
+	h = 0;
+	this.options.element.width(w);
+	scrollPane.children(this.options.itemsSelector).children("li").each(function() {
+		var item, itemHeight;
+		item = tc.jQ(this);
+		item.width(w);
+		itemHeight = item.height();
+		if (itemHeight > h) {
+			h = itemHeight;
+		}
+	});
+	scrollPane.height(h);
+	
+	scrollPane.scrollable({
 		speed: 300,
 		circular: true
 	});
-	this.carousel = this.options.element.data("scrollable");
+	this.carousel = scrollPane.data("scrollable");
 };
