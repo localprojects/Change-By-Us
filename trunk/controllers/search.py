fc58c6cb45760c700e1e0f45930c21062a70609a
ethan <ethan@befef031-aaeb-48d0-b70f-681328309d3d>
2011-03-14 22:45:04 +0000

    new map data format and location info
    git-svn-id: http://svn.localprojects.net/gam2@200 befef031-aaeb-48d0-b70f-681328309d3d

--- trunk/controllers/search.py
+++ trunk/controllers/search.py
@@ -8,16 +8,33 @@ class Search(Controller):
             return self.render('map', {'neighborhoodJSON':self.getLocationJSON()})
         else:
             log.info("*** render search")
-            return self.render('search')   
+            return self.render('search')
+            
+    def POST(self, action=None):
+        if (action == 'info'):
+            return self.getLocationInfoJSON()
+        else:
+            return self.not_found()
             
     def getLocationJSON(self):
         data = mLocation.getLocationsWithScoring(self.db)
         
-        locations = {}
+        locations = []
         
         for item in data:
-            locations[item.name] = {'location_id':item.location_id, 'lat':str(item.lat), 'lon':str(item.lon), 'score': (item.score if item.score else 0)}
+            locations.append(dict(name = item.name, 
+                                location_id = item.location_id, 
+                                lat = str(item.lat), 
+                                lon = str(item.lon),
+                                score = (item.score if item.score else 0)))
             
-        return self.json(dict(locations))            
+        return self.json(locations)
+        
+    def getLocationInfoJSON(self):
+        locationId = self.request('location_id')
+    
+        info = mLocation.getLocationInfo(self.db, locationId)
+        
+        return self.json(info)
             
             
