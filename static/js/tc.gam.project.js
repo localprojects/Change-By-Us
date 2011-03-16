2a6df117d0f911ded7d2436813e3f6ed2534f87e
Lev Kanter <lev@typeslashcode.com>
2011-03-16 22:24:22 +0000

    working on show hide of widgets on project page
    git-svn-id: http://svn.localprojects.net/gam2/trunk@225 befef031-aaeb-48d0-b70f-681328309d3d

--- static/js/tc.gam.project.js
+++ static/js/tc.gam.project.js
@@ -3,6 +3,8 @@ if(!tc.gam){ tc.gam = {}; }
 
 
 tc.gam.project = function(options){
+	var me;
+	me = this;
 	
 	this.options = tc.jQ.extend({
 		
@@ -13,14 +15,16 @@ tc.gam.project = function(options){
 	this.widget = new tc.gam.widget(null,this);
 	
 	this.components = {
-		infopane:new tc.gam.project_widgets.infopane(this,this.dom.find('.mission'),{widget:this.widget},{}),
-		members:new tc.gam.project_widgets.members(this,this.dom.find('.box.members'),{widget:this.widget},{}),
-		resources:new tc.gam.project_widgets.resources(this,this.dom.find('.members'),{widget:this.widget},{}),
-		goals:new tc.gam.project_widgets.goals(this,this.dom.find('.goals'),{widget:this.widget},{}),
+		infopane:new tc.gam.project_widgets.infopane(this,this.dom.find('.box.mission'),{widget:this.widget},{}),
+		resources:new tc.gam.project_widgets.resources(this,this.dom.find('.box.resources'),{widget:this.widget},{}),
+		related_ideas:new tc.gam.project_widgets.fresh_ideas(this,this.dom.find('.box.fresh-ideas'),{widget:this.widget},{}),
+		
+		related_resources:new tc.gam.project_widgets.related_resources(this,this.dom.find('.box.related_resources'),{widget:this.widget},{}),
+		goals:new tc.gam.project_widgets.goals(this,this.dom.find('.box.goals'),{widget:this.widget},{}),
 		goals_add:new tc.gam.project_widgets.goals_add(this,this.dom.find('.goals-add'),{widget:this.widget},{}),
-		conversation:new tc.gam.project_widgets.conversation(this,this.dom.find('.conversation'),{widget:this.widget},{}),
-		related_ideas:new tc.gam.project_widgets.fresh_ideas(this,this.dom.find('.fresh-ideas'),{widget:this.widget},{})
-	}
+		conversation:new tc.gam.project_widgets.conversation(this,this.dom.find('.box.conversation'),{widget:this.widget},{}),
+		members:new tc.gam.project_widgets.members(this,this.dom.find('.box.members'),{widget:this.widget},{})
+	};
 	
 	this.handlers = {
 		members_button_clicked:function(e,d){
@@ -56,7 +60,7 @@ tc.gam.project = function(options){
 					break;
 			}
 		}
-	}
+	};
 	
 	this.dom.find('.project-header .members a').bind('click',this.event_data,this.handlers.members_button_clicked);
 	this.dom.bind('project-widget-show',this.event_data,this.handlers.widget_show);
@@ -66,112 +70,131 @@ tc.gam.project = function(options){
 
 
 tc.gam.widget = function(inheritor,project){
-  if(!inheritor){ return; }
+	if(!inheritor){ return; }
+	
 	inheritor.show = function(propagate){
-	  if(inheritor.dom){ inheritor.dom.show(); }
+		if(inheritor.dom){ inheritor.dom.show(); }
 		if(propagate !== false){
-      project.dom.trigger('project-widget-show',{name:inheritor.options.name});
+			project.dom.trigger('project-widget-show',{name:inheritor.options.name});
 		}
-		
 	};
+	
 	inheritor.hide = function(propagate){
-    if(inheritor.dom){ inheritor.dom.hide(); }
-    if(propagate !== false){
-      project.dom.trigger('project-widget-hide',{name:inheritor.options.name}); 
-    }
+		if(inheritor.dom){ inheritor.dom.hide(); }
+		if(propagate !== false){
+			project.dom.trigger('project-widget-hide',{name:inheritor.options.name});
+		}
 	};
 	
 	return inheritor;
-	
-}
+};
 
 tc.gam.project_widgets = {};
 
 tc.gam.project_widgets.infopane = function(project,dom,deps,options){
-  var widget;
-  this.options = tc.jQ.extend({name:'infopane'},options);
-  this.dom = dom;
-  widget = tc.gam.widget(this,project);
-  return {
-    show:widget.show,
-    hide:widget.hide
-  }
-}
-tc.gam.project_widgets.members = function(project,dom,deps,options){
-  var widget;
-  this.options = tc.jQ.extend({name:'members'},options);
-  this.dom = dom;
-  widget = tc.gam.widget(this,project);
-  this.handlers = {
-    back:function(e,d){
-      e.data.me.hide(true);
-    }
-  };
-  this.dom.find('.actions.back').bind('click',{ project:project,me:this },this.handlers.back);
-  return {
-    show:widget.show,
-    hide:widget.hide
-  }
-}
+	var widget;
+	this.options = tc.jQ.extend({name:'infopane'},options);
+	this.dom = dom;
+	widget = tc.gam.widget(this,project);
+	return {
+		show:widget.show,
+		hide:widget.hide
+	}
+};
+
 tc.gam.project_widgets.resources = function(project,dom,deps,options){
-  var widget;
-  this.options = tc.jQ.extend({name:'resources'},options);
-  this.dom = dom;
-  widget = tc.gam.widget(this,project);
-  return {
-    show:widget.show,
-    hide:widget.hide
-  }
-}
-tc.gam.project_widgets.goals = function(project,dom,deps,options){
-  var widget;
-  this.options = tc.jQ.extend({name:'goals'},options);
-  this.dom = dom;
-  widget = tc.gam.widget(this,project);
-  this.handlers = {
-    add_goal:function(e,d){
-      e.data.project.components.goals_add.show();
-    }
-  };
-  this.dom.find('.actions .add').bind('click',{ project:project,me:this },this.handlers.add_goal);
-  return {
-    show:widget.show,
-    hide:widget.hide
-  }
-}
-tc.gam.project_widgets.goals_add = function(project,dom,deps,options){
-  var widget;
-  this.options = tc.jQ.extend({name:'goals_add'},options);
-  this.dom = dom;
-  widget = tc.gam.widget(this,project);
-  this.handlers = {
-    back:function(e,d){
-      e.data.project.components.goals_add.hide(true);
-    }
-  };
-  this.dom.find('.actions.back a').bind('click',{ project:project,me:this },this.handlers.back);
-  return {
-    show:widget.show,
-    hide:widget.hide
-  }
-}
-tc.gam.project_widgets.conversation = function(project,dom,deps,options){
-  var widget;
-  this.options = tc.jQ.extend({name:'conversation'},options);
-  this.dom = dom;
-  widget = tc.gam.widget(this,project);
-  return {
-    show:widget.show,
-    hide:widget.hide
-  }
-}
+	var widget;
+	this.options = tc.jQ.extend({name:'resources'},options);
+	this.dom = dom; 
+	widget = tc.gam.widget(this,project);
+	return {
+		show:widget.show,
+		hide:widget.hide
+	}
+};
+
 tc.gam.project_widgets.fresh_ideas = function(project,dom,deps,options){
-  var widget;
-  this.options = tc.jQ.extend({name:'infopane'},options);
-  this.dom = dom;
-  widget = tc.gam.widget(this,project);
-  return {
-    show:widget.show,
-    hide:widget.hide
-  }
-}
+	var widget;
+	this.options = tc.jQ.extend({name:'infopane'},options);
+	this.dom = dom;
+	widget = tc.gam.widget(this,project);
+	return {
+		show:widget.show,
+		hide:widget.hide
+	}
+};
+
+tc.gam.project_widgets.related_resources = function(project,dom,deps,options){
+	var widget;
+	this.options = tc.jQ.extend({name:'related_resources'},options);
+	this.dom = dom; 
+	widget = tc.gam.widget(this,project);
+	return {
+		show:widget.show,
+		hide:widget.hide
+	}
+};
+
+tc.gam.project_widgets.goals = function(project,dom,deps,options){
+	var widget;
+	this.options = tc.jQ.extend({name:'goals'},options);
+	this.dom = dom;
+	widget = tc.gam.widget(this,project);
+	this.handlers = {
+		add_goal:function(e,d){
+			e.data.project.components.goals_add.show();
+		}
+	};
+	this.dom.find('.actions .add').bind('click',{ project:project,me:this },this.handlers.add_goal);
+	return {
+		show:widget.show,
+		hide:widget.hide
+	}
+};
+
+tc.gam.project_widgets.goals_add = function(project,dom,deps,options){
+	var widget; 
+	this.options = tc.jQ.extend({name:'goals_add'},options);
+	this.dom = dom;
+	widget = tc.gam.widget(this,project); 
+	this.handlers = {
+		back:function(e,d){
+			e.preventDefault();
+			e.data.project.components.goals_add.hide(true);
+		}
+	};
+	this.dom.find('.actions a.back').bind('click',{ project:project,me:this },this.handlers.back);
+	return {
+		show:widget.show,
+		hide:widget.hide
+	}
+};
+
+tc.gam.project_widgets.conversation = function(project,dom,deps,options){
+	var widget;
+	this.options = tc.jQ.extend({name:'conversation'},options);
+	this.dom = dom;
+	widget = tc.gam.widget(this,project);
+	return { 
+		show:widget.show,
+		hide:widget.hide
+	}
+};
+
+tc.gam.project_widgets.members = function(project,dom,deps,options){
+	var widget;
+	this.options = tc.jQ.extend({name:'members'},options);
+	this.dom = dom;
+	widget = tc.gam.widget(this,project);
+	this.handlers = {
+		back:function(e,d){
+			e.preventDefault();
+			e.data.me.hide(true);
+		}
+	};
+	this.dom.find('.actions a.back').bind('click',{ project:project,me:this },this.handlers.back);
+	return {
+		show:widget.show,
+		hide:widget.hide
+	}
+};
