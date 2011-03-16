5f408d662243854c8bc8faaac36f095f31f2ad58
ethan <ethan@befef031-aaeb-48d0-b70f-681328309d3d>
2011-03-16 20:18:58 +0000

    Dummied up search data
    git-svn-id: http://svn.localprojects.net/gam2@223 befef031-aaeb-48d0-b70f-681328309d3d

--- trunk/controllers/search.py
+++ trunk/controllers/search.py
@@ -1,5 +1,6 @@
 import framework.util as util
 import giveaminute.location as mLocation
+import giveaminute.project as mProject
 from framework.controller import *
 
 class Search(Controller):
@@ -7,7 +8,7 @@ class Search(Controller):
         if action == 'map':
             return self.showMap()
         else:
-            return self.render('search')
+            return self.showSearch()   
                         
     def showMap(self):
         locationData = self.getLocationData()
@@ -16,6 +17,18 @@ class Search(Controller):
         self.template_data['max_score'] = locationData[0]['score']
     
         return self.render('map')
+        
+    def showSearch(self):
+        # dummied up from real data
+        projects = mProject.getProjectsByKeywords(self.db, 'bicycle')
+        resources = self.getDummyResources()        
+        ideas = self.getDummyIdeas()
+        
+        results = dict(projects = projects, resources = resources, ideas = ideas)
+        
+        self.template_data['results'] = dict(json = self.json(results))
+        
+        return self.render('search')
             
     def getLocationData(self):
         data = mLocation.getLocationsWithScoring(self.db)
@@ -42,5 +55,53 @@ class Search(Controller):
         score = numProjects + numIdeas + numResources
         
         return score 
+
+    # temp dummy functions
+    def getDummyResources(self):
+        limit = 10
+        sql = "select project_resource_id as link_id, title, url, image_id from project_resource limit $limit"
+        data = list(self.db.query(sql, {'limit':limit}))
+                
+        return data
+        
+    def getDummyIdeas(self):
+        limit = 10
+        sql = """
+select i.idea_id
+       ,i.description
+      ,i.submission_type
+      ,i.created_datetime
+      ,u.user_id
+      ,u.first_name
+      ,u.last_name
+from idea i
+left join user u on u.user_id = i.user_id limit $limit"""
+
+        data = list(self.db.query(sql, {'limit':limit}))
+        
+        betterData = []
+        
+        for item in data:
+            owner = None
             
-            
+            if (item.user_id):
+                name = "%s %s." % (item.first_name, item.last_name[0])
+                
+                owner = dict(name = name, user_id = item.user_id)
+        
+            betterData.append(dict(idea_id = item.idea_id,
+                            message = item.description,
+                            created = str(item.created_datetime),
+                            submission_type = item.submission_type,
+                            owner = owner))
+                            
+        log.info("*** %s" % str(betterData))
+                
+        return betterData
+        
+        
+        
+        
+        
+        
+        
