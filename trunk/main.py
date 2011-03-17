f22da09ff3fefe9596a46062936bfbc6aae84cba
ethan <ethan@befef031-aaeb-48d0-b70f-681328309d3d>
2011-03-17 13:59:10 +0000

    project additions: - must be logged in to create - live project data - endorsements
    git-svn-id: http://svn.localprojects.net/gam2@237 befef031-aaeb-48d0-b70f-681328309d3d

--- trunk/main.py
+++ trunk/main.py
@@ -31,6 +31,7 @@ sys.path.append("lib/")
 ROUTES = (  r'/create/?([^/.]*)', 'controllers.createProject.CreateProject',
             r'/idea/?([^/.]*)/?([^/.]*)', 'controllers.idea.Idea',
             r'/join/?([^/.]*)', 'controllers.join.Join',
+            r'/project/?([^/.]*)/?([^/.]*)', 'controllers.project.Project',
             r'/search/?([^/.]*)', 'controllers.search.Search',
             r'/?([^/.]*)', 'controllers.home.Home' )
 
