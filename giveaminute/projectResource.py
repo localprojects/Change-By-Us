126a968d30466faf0d4525f483e267271468d96d
Ethan Holda <ethan@localprojects.net>
2011-03-17 17:35:59 +0000

    resource info service
    git-svn-id: http://svn.localprojects.net/gam2/trunk@243 befef031-aaeb-48d0-b70f-681328309d3d

--- giveaminute/projectResource.py
+++ giveaminute/projectResource.py
@@ -3,6 +3,33 @@ from framework.log import log
 class ProjectResource():
     def __init__(self, db, projectResourceId):
         self.id = projectResourceId
+        self.db = db
+        self.data = self.populateResourceData()
+        
+    def populateResourceData(self):
+        sql = """select project_resource_id, title, description, url, contact_name, contact_email, image_id, location_id
+                from project_resource where project_resource_id = $id;"""
+        
+        try:
+            data = list(self.db.query(sql, {'id':self.id}))
+            
+            if len(data) > 0:
+                return data[0]
+            else:
+                return None
+        except Exception, e:
+            log.info("*** couldn't get project resource info")
+            log.error(e)
+            return None 
+            
+    def getFullDictionary(self):
+        data = dict(image_id = self.data.image_id,
+                    project_resource_id = self.data.project_resource_id,
+                    description = self.data.description,
+                    title = self.data.title,
+                    url = self.data.url,
+                    location_id = self.data.location_id)
+        return data
         
 def getProjectResourcesByLocation(db, locationId):
     try:
