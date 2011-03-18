9c5209b91ee0019cedf87502f9d9a36cabed8734
ethan <ethan@befef031-aaeb-48d0-b70f-681328309d3d>
2011-03-18 18:17:35 +0000

    live data on homepage
    git-svn-id: http://svn.localprojects.net/gam2@294 befef031-aaeb-48d0-b70f-681328309d3d

--- trunk/controllers/home.py
+++ trunk/controllers/home.py
@@ -27,8 +27,17 @@ class Home(Controller):
             
     def showHome(self):
         #temp fix
+#         locations = mLocation.getSimpleLocationDictionary(self.db)
+#         allIdeas = self.getAllProjectIdeas();
+#         
+#         locationsObj = dict(data = locations, json = self.json(locations))
+#         allIdeasObj = dict(data = allIdeas, json = self.json(allIdeas))
+        
+#         self.template_data['locations'] = locationsObj
+#         self.template_data['all_ideas'] = allIdeasObj
+        
         locations = dict(data = mLocation.getSimpleLocationDictionary(self.db), json = self.json(mLocation.getSimpleLocationDictionary(self.db)))
-        allIdeas = dict(data = self.getAllProjectIdeas(), json = self.json(self.getAllProjectIdeas()))
+        allIdeas = dict(data = self.getFeaturedProjectIdeas(), json = self.json(self.getFeaturedProjectIdeas()))
         
         self.template_data['locations'] = locations
         self.template_data['all_ideas'] = allIdeas
@@ -69,54 +78,44 @@ class Home(Controller):
 
         return True    
         
-    def getAllProjectIdeas(self):
+    def getFeaturedProjectIdeas(self):
         data = []
         
-        data.append(self.getProjectIdeas(1, 50))
-        data.append(self.getProjectIdeas(2, 30))
-        data.append(self.getProjectIdeas(3, 30))
-        data.append(self.getProjectIdeas(4, 30))
-        data.append(self.getProjectIdeas(5, 15))
+        sql = """select p.project_id, p.title from project p
+                inner join featured_project fp on fp.project_id = p.project_id order by fp.ordinal"""
+        
+        featured = list(self.db.query(sql))
+        
+        for project in featured:
+            data.append(dict(title = str(project.title),
+                            ideas = self.getProjectIdeas(project.project_id, 30)))   
         
         return data
         
-    def getProjectIdeas(self, index, num):
-        data = dict(title = '',ideas = [])
+    def getProjectIdeas(self, projectId, limit):
+        data = []
+        betterData = []
         
-        if index == 1:
-          data['title'] = 'More Trees in NYC'
-          for i in range(num):
-            data['ideas'].append(dict(text = "More Trees in New York City would be awesome and it would totally help to make it way more green! Let's plant some!",
-                            f_name = "John",
-                            l_name = "Smith",
-                            submitted_by = "web"))
-        elif index == 2:
-          data['title'] = 'Outta There'
-          for i in range(num):
-            data['ideas'].append(dict(text = "Phew, this Austin place is way nicer anyways.",
-                            f_name = "Ethan",
-                            l_name = "Holda",
-                            submitted_by = "web"))
-        elif index == 3:
-          data['title'] = 'Continuous Climbing'
-          for i in range(num):
-            data['ideas'].append(dict(text = "There arent enough opportunities to climb in this city. Lets turn the whole place into a gym by selling tower climbing permits.",
-                            f_name = "Zeke",
-                            l_name = "Shore",
-                            submitted_by = "web"))
-        elif index == 4:
-          data['title'] = 'French'
-          for i in range(num):
-            data['ideas'].append(dict(text = "This place needs more culture, more joie de vivre. Lets import some culture experts from Paris.",
-                            f_name = "Lev",
-                            l_name = "Kanter",
-                            submitted_by = "web"))
-        elif index == 5:
-          data['title'] = 'Clean Up Canal'
-          for i in range(num):
-            data['ideas'].append(dict(text = "The bag-sellers outside my apartment are terrible. Out with the bags, in with the schwarma.",
-                            f_name = "Ian",
-                            l_name = "Lord",
-                            submitted_by = "web"))
+        sql = """select i.idea_id, i.description as text, u.first_name as f_name, u.last_name as l_name, i.submission_type as submitted_by 
+                from idea i
+                inner join project__idea pi on pi.idea_id = i.idea_id and pi.project_id = $id
+                left join user u on u.user_id = i.user_id
+                limit $limit"""
+                
+        log.info("*** proj idea sql = %s" % sql)
+                
+        try:
+            data = list(self.db.query(sql, {'id':projectId, 'limit':limit}))
+        
+            for item in data:
+                betterData.append(dict(text = str(item.text),
+                            f_name = str(item.f_name) if item.f_name else '',
+                            l_name = str(item.l_name) if item.l_name else '',
+                            submitted_by =  str(item.submitted_by)))   
+        
+        except Exception, e:
+            log.info("*** couldn't get project ideas for home page")
+            log.error(e)    
             
-        return data
+        return betterData
+                
