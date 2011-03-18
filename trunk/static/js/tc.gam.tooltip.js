3376740cd7b393dec69d0b4584ee8aaf7eb4b212
amahon <amahon@befef031-aaeb-48d0-b70f-681328309d3d>
2011-03-18 19:59:11 +0000

    more fun stuff
    git-svn-id: http://svn.localprojects.net/gam2@303 befef031-aaeb-48d0-b70f-681328309d3d

--- trunk/static/js/tc.gam.tooltip.js
+++ trunk/static/js/tc.gam.tooltip.js
@@ -3,23 +3,46 @@ if (!tc) { var tc = {}; }
 tc.resource_tooltip = makeClass();
 
 tc.resource_tooltip.prototype.options = {
-	element: null,
+	trigger: null,
 	get_url: null,
 	get_params: null
 };
 
 tc.resource_tooltip.prototype.tooltip = null;
 
+
 tc.resource_tooltip.prototype.init = function(options) {
 	var me;
 	tc.util.log("tc.resource_tooltip.init");
 	me = this;
 	this.options = tc.jQ.extend(this.options, options);
-		
-	this.options.element.tooltip({
+	this.build_tooltip();
+	this.options.trigger.bind('mouseover',{me:this},this.handlers.mouseover);
+	this.options.trigger.bind('mouseout',{me:this},this.handlers.mouseout);
+};
+
+tc.resource_tooltip.prototype.handlers = {
+	mouseover:function(e){
+		e.data.me.tooltip.show();
+	},
+	mouseout:function(e){
+		e.data.me.tooltip.hide();
+	}
+}
+
+tc.resource_tooltip.prototype.build_tooltip = function(){
+	tc.util.log("tc.resource_tooltip.build_tooltip");
+	var me;
+	me = this;
+	this.tooltip = this.options.trigger.tooltip({
 		position: "center right",
-		offset: [0, -20],
+		offset: [0, 20],
 		predelay: 100,
+		tip:'.template-content.tooltip',
+		events: {
+			def: ",",    // default show/hide events for an element
+			tooltip: ","     // the tooltip element
+		},
 		onShow: function(e) {
 			var tip, trig;
 			tip = this.getTip();
@@ -27,43 +50,26 @@ tc.resource_tooltip.prototype.init = function(options) {
 			
 			if (!(tip.hasClass("has-been-loaded"))) {
 				tip.html('<div class="tooltip-bd spinner"><img class="loading" src="/static/images/loader32x32.gif" /></div>');
+				tip.addClass('has-been-loaded');
+				tc.util.dump(me.options);
 				tc.jQ.ajax({
 					url: me.options.get_url,
 					data: me.options.get_params,
-					context: tip,
+					context: this,
 					dataType:'json',
 					success:function(data,ts,xhr){
 						var html;
-						//tc.util.dump(data);
-						
-						html = 
-							"<div class='tooltip-hd'>"+ 
-								"<h2>"+ data.title +"</h2>"+
-							"</div>"+
-							"<div class='tooltip-bd'>"+
-								"<div class='info'>"+
-									"<div class='thumb'>"+
-										"<img alt='"+ data.title + "' src='http://placehold.it/90x90'/>"+
-									"</div>"+
-									"<div class='main'>"+
-										"<p>"+ data.description +"</p>"+
-									"</div>"+
-								"</div>"+
-								"<dl class='details'>"+
-									"<dt>Visit Us</dt>"+
-									"<dd><a href='"+ data.url +"'>"+ data.url +"</a></dd>"+
-								"</dl>"+
-							"</div>";
-						
-						this.html(html).addClass("has-been-loaded");
+						tc.util.dump(data);
+						//this.getTip().html(html);//.addClass("has-been-loaded");
 					}
 				});
 			}
 		},
+		onBeforeHide:function(e){
+			return true;
+		},
 		onHide: function(e) {
 			
 		}
-	}).dynamic( {} );
-	
-	this.tooltip = this.options.element.data("tooltip");
-};
+	}).data("tooltip");//.dynamic( {} );
+}
