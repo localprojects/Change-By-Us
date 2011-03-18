0c6337b55c337b2c77dc516f5782c158538a5472
Lev Kanter <lev@typeslashcode.com>
2011-03-18 23:23:40 +0000

    added topbar js to encapsulate topvar functionality
    git-svn-id: http://svn.localprojects.net/gam2/trunk@316 befef031-aaeb-48d0-b70f-681328309d3d

---
+++ static/js/tc.gam.topbar.js
@@ -0,0 +1,23 @@
+if (!tc) { var tc = {}; }
+
+tc.top_bar = function(element, options) {
+	var o;
+	
+	o = tc.jQ.extend({
+		slideSpeed: 250,
+		fadeSpeed: 200
+	}, options);
+	
+	function init() {
+		element.find(".username, .myprojects").mouseenter(function () {
+			tc.jQ(this).children(".dropdown").stop(true, true).slideDown(o.slideSpeed);
+		}).mouseleave(function () {
+			tc.jQ(this).children(".dropdown").fadeOut(o.fadeSpeed);
+		});
+	}
+	
+	init();
+	return {
+		
+	};
+}
