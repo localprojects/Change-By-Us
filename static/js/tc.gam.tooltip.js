795f2f9922afeb81fb55b6bacb7117f6e50a51aa
Andrew Mahon <andrew@typeslashcode.com>
2011-03-19 01:48:03 +0000

    git-svn-id: http://svn.localprojects.net/gam2/trunk@330 befef031-aaeb-48d0-b70f-681328309d3d

--- static/js/tc.gam.tooltip.js
+++ static/js/tc.gam.tooltip.js
@@ -17,16 +17,23 @@ tc.resource_tooltip.prototype.init = function(options) {
 	me = this;
 	this.options = tc.jQ.extend(this.options, options);
 	this.build_tooltip();
+	this.cached_data = {};
 	this.options.trigger.bind('mouseover',{me:this},this.handlers.mouseover);
 	this.options.trigger.bind('mouseout',{me:this},this.handlers.mouseout);
 };
 
 tc.resource_tooltip.prototype.handlers = {
 	mouseover:function(e){
-		e.data.me.tooltip.show();
+		tc.util.dump(e.target.nodeName);
+		if(e.target.nodeName == 'TD'){
+			e.data.me.tooltip.show();
+		}
 	},
 	mouseout:function(e){
-		e.data.me.tooltip.hide();
+		tc.util.dump(e.target.nodeName);
+		if(e.target.nodeName == 'TD'){
+			e.data.me.tooltip.hide();
+		}
 	}
 }
 
@@ -35,37 +42,53 @@ tc.resource_tooltip.prototype.build_tooltip = function(){
 	var me;
 	me = this;
 	this.tooltip = this.options.trigger.tooltip({
-		position: "center right",
-		offset: [0, 20],
+		position: "",
+		offset: [0, 0],
 		predelay: 100,
 		tip:'.template-content.tooltip',
 		events: {
 			def: ",",    // default show/hide events for an element
 			tooltip: ","     // the tooltip element
 		},
-		onShow: function(e) {
-			var tip, trig;
+		onBeforeShow: function(e) {
+			var tip, trig, target_pos;
 			tip = this.getTip();
+			tip.css('position','fixed').show();
 			trig = this.getTrigger();
+			target_pos = {
+				top:this.getTrigger().offset().top+this.getTrigger().height(),
+				left:this.getTrigger().offset().left+this.getTrigger().width()
+			}
 			
-			if (!(tip.hasClass("has-been-loaded"))) {
-				tip.html('<div class="tooltip-bd spinner"><img class="loading" src="/static/images/loader32x32.gif" /></div>');
-				tip.addClass('has-been-loaded');
-				tc.util.dump(me.options);
+			tip.html('<div class="tooltip-bd spinner"><img class="loading" src="/static/images/loader32x32.gif" /></div>');
+			
+			tc.util.dump(this.getTip().data('cached-data'));
+			if(this.getTip().data('cached-data')){
+				this.getTip().animate({
+					'top':target_pos.top,
+					'left':target_pos.left
+				},1000,'easeOutElastic');
+			} else {
 				tc.jQ.ajax({
 					url: me.options.get_url,
 					data: me.options.get_params,
+					async:false,
 					context: this,
 					dataType:'json',
 					success:function(data,ts,xhr){
-						var html;
-						tc.util.dump(data);
-						//this.getTip().html(html);//.addClass("has-been-loaded");
+						this.getTip().data('cached-data',data);
 					}
 				});
+				this.getTip().animate({
+					'top':target_pos.top-50,
+					'left':target_pos.left
+				},1000,'easeOutElastic').text(this.getTip().data('cached-data'));
 			}
+			
+			return false;
 		},
 		onBeforeHide:function(e){
+			//this.getTip().hide();
 			return true;
 		},
 		onHide: function(e) {
