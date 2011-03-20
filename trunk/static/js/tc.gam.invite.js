1f7a5ed41d7611a729c68bbb772b77922d2c0279
lkanter <lkanter@befef031-aaeb-48d0-b70f-681328309d3d>
2011-03-20 21:33:32 +0000

    starting to generalize ideas invite so that it can be used on search results page
    git-svn-id: http://svn.localprojects.net/gam2@375 befef031-aaeb-48d0-b70f-681328309d3d

---
+++ trunk/static/js/tc.gam.invite.js
@@ -0,0 +1,92 @@
+if(!tc){ var tc = {}; }
+if(!tc.gam){ tc.gam = {}; }
+
+tc.gam.ideas_invite = function(app) {
+	// Invite
+	tc.jQ('a.invite').bind('click',{
+		app:app,
+		source_element:tc.jQ('.modal-content.invite-user'),
+		init:function(modal,event_target,callback){
+			var modal_merlin;
+			modal_merlin = new tc.merlin(app,{
+				name:'ideas_invite',
+				dom:modal.options.element.find('.invite-user'),
+				first_step:'invite-message-info',
+				data:{
+					project_id: null,
+					idea_id: null,
+					message: ""
+				},
+				steps:{
+					'invite-message-info':{
+						selector:'.invite-message-info',
+						inputs:{
+							invite_message:{
+								selector:'textarea.invite-message',
+								validators:['max-200']
+							}
+						},
+						init:function(merlin,dom){
+							dom.find(".name").text( tc.jQ(event_target).attr("href").split(",")[2] );
+								
+							dom.find('a.submit').bind('click',{merlin:merlin,dom:dom},function(e,d){
+								e.preventDefault();
+								if(dom.hasClass('invalid')){
+									return;
+								}
+								e.data.merlin.show_step('invite-message-submit');
+							});
+						},
+						finish: function(merlin, dom) {
+							var message;
+							message = merlin.current_step.inputs.invite_message.dom.val();
+							
+							merlin.options.data = tc.jQ.extend(merlin.options.data, {
+								project_id:merlin.app.app_page.data.project.project_id,
+								idea_id: tc.jQ(event_target).attr("href").split(",")[1],
+								message: message
+							});
+						}
+					},
+					'invite-message-project-info':{
+						selector:'.invite-message-project-info',
+						inputs:{
+							
+						},
+						init: function(merlin, dom) {
+							dom.find(".name").text( tc.jQ(event_target).attr("href").split(",")[2] );
+						},
+						finish: function(merlin, dom) {
+							
+						}
+					},
+					'invite-message-submit':{
+						selector:'.invite-message-submit',
+						init: function(merlin, dom) {
+							tc.jQ.ajax({
+								type:"POST",
+								url:"/project/invite",
+								data:merlin.options.data,
+								context:merlin,
+								dataType:"text",
+								success: function(data, ts, xhr) {
+									if (data == "False") {
+										return false;
+									}
+									tc.timer(1000, function() {
+										modal.hide();
+									});
+								}
+							});
+						}
+					}
+				}
+			});
+			if(tc.jQ.isFunction(callback)){
+				callback(modal);
+			}
+		}
+	},function(e,d){
+		e.data.app.components.modal.show(e.data, e.target);
+	});
+};
