3f5cbdc6a68e673ca02414fc3bfdb44a44514528
lkanter <lkanter@befef031-aaeb-48d0-b70f-681328309d3d>
2011-03-25 02:17:45 +0000

    working on changing invite button to say invited after inviting a person
    git-svn-id: http://svn.localprojects.net/gam2@450 befef031-aaeb-48d0-b70f-681328309d3d

--- trunk/static/js/tc.gam.invite.js
+++ trunk/static/js/tc.gam.invite.js
@@ -94,6 +94,7 @@ tc.gam.ideas_invite = function(app) {
 									if (data == "False") {
 										return false;
 									}
+									tc.jQ(event_target).addClass("invited").text("Invited");
 									tc.timer(1000, function() {
 										modal.hide();
 									});
