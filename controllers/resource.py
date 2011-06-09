19c407dfe127815179ddc8d95f8c4f7816847d06
Ethan Holda <ethan@localprojects.net>
2011-06-09 20:21:59 +0000

    fixed issue of passing makeUrlAbsolute a null value
    git-svn-id: http://svn.localprojects.net/gam2/trunk@1176 befef031-aaeb-48d0-b70f-681328309d3d

--- controllers/resource.py
+++ controllers/resource.py
@@ -24,13 +24,13 @@ class Resource(Controller):
         description = self.request('description')
         physical_address = self.request('physical_address')
         location_id = util.try_f(int, self.request('location_id'), -1)
-        url = util.makeUrlAbsolute(self.request('url'))
+        url = util.makeUrlAbsolute(self.request('url')) if self.request('url')  else None
         keywords = ' '.join([word.strip() for word in self.request('keywords').split(',')]) if not util.strNullOrEmpty(self.request('keywords')) else None
         contact_name = self.request('contact_name')
         contact_email = self.request('contact_email')
-        facebook_url = util.makeUrlAbsolute(self.request('facebook_url'))
-        twitter_url = util.makeUrlAbsolute(self.request('twitter_url'))
-        image_id = util.try_f(int, self.request('image'))
+        facebook_url = util.makeUrlAbsolute(self.request('facebook_url')) if self.request('facebook_url') else None
+        twitter_url = util.makeUrlAbsolute(self.request('twitter_url')) if self.request('twitter_url') else None
+        image_id = util.try_f(int, self.request('image')) 
         
         # TODO this is a temp fix for a form issue
         if (contact_name == 'null'):
