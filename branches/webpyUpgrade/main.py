f847b4bfff9d5112ac4a919d1bc6159d4abc88c6
sraman <sraman@befef031-aaeb-48d0-b70f-681328309d3d>
2011-06-12 04:42:24 +0000

    Corrects a typo with how web config was set
    git-svn-id: http://svn.localprojects.net/gam2@1230 befef031-aaeb-48d0-b70f-681328309d3d

--- branches/webpyUpgrade/main.py
+++ branches/webpyUpgrade/main.py
@@ -37,9 +37,9 @@ if __name__ == "__main__":
     if Config.get('email').get('smtp'):
         smtp_config = Config.get('email').get('smtp')
         web.webapi.config.smtp_server = smtp_config.get('host')
-        web.config.smtp_port = smtp_config.get('port')
-        web.config.smtp_username = smtp_config.get('username')
-        web.config.smtp_password = smtp_config.get('password')
+        web.webapi.config.smtp_port = smtp_config.get('port')
+        web.webapi.config.smtp_username = smtp_config.get('username')
+        web.webapi.config.smtp_password = smtp_config.get('password')
 
     elif Config.get('email').get('aws_ses'):
         # AWS SES config
