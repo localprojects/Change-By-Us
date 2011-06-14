71f8242ce545b51b94cfd0b3239881374314e023
sraman <sraman@befef031-aaeb-48d0-b70f-681328309d3d>
2011-06-14 17:03:14 +0000

    WIP: Creation of the digest emails
    git-svn-id: http://svn.localprojects.net/gam2@1265 befef031-aaeb-48d0-b70f-681328309d3d

--- branches/webpyUpgrade/scripts/digest_emailer.py
+++ branches/webpyUpgrade/scripts/digest_emailer.py
@@ -4,90 +4,166 @@
 #
 # 
 
-def get_recent_messages(db, projectId, fromDate=None, filterBy='member_comment'):
+import yaml
+import os, sys
+sys.path.append("../lib/")
+
+import web
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+class Configurable():
+    def loadConfigs(self, config_file, section=None):
+        if not os.path.exists(config_file):
+            raise IOError("Could not open %s" % config_file)
+
+        f = open(config_file)
+        conf = yaml.load(f)
+        f.close()
+        if section:
+            return conf.get(section)
+        else:
+            return conf
+     
+class WebpyDBConnectable():
     """
-    from: datetime of last digest
+    Provide database connections stuff
     """
-    messages = []
-    if (filterBy not in ['member_comment','admin_comment','goal_achieved','join','endorsement']):
-        filterBy = None
-    sql = """
-select 
-    pm.project_message_id,
-    pm.message_type,
-    pm.message,
-    pm.created_datetime,
+
+    DBHandle = None
+
+    # Library functions (internal only)
+    def connectDB(self, dbParams):
+        self.DBHandle = web.database(dbn=dbParams.get('dbn'),user=dbParams.get('user'), pw=dbParams.get('password'), db=dbParams.get('db'), host=dbParams.get('host'))
+        # self.DBHandle = web.db.connect( host=dbParams.get('host'), user=dbParams.get('user'), 
+        #                                  passwd=dbParams.get('password'), db=dbParams.get('db') )
+
+    def disconnectDB(self):
+        if self.DBHandle is not None:
+            self.DBHandle.close()
+
+    def executeSQL(self, sql, params):
+        return self.DBHandle.query(sql, params)
+
+class GiveAMinuteDigest(Configurable, WebpyDBConnectable):
+    def __init__(self, configFile=None):
+        print "Will load from config file %s" % configFile
+        # Connect to the mysql database based on the params from Config.yaml
+        dbParams = self.loadConfigs(config_file=configFile, section='database')
+        self.connectDB(dbParams)
+
+    # def __del__(self):
+    #     self.disconnectDB()
+        
+    # Publicly visible functions
+
+    def getRecentMessages(self, projectId=None, fromDate=None, filterBy='member_comment'):
+        """
+        from: datetime of last digest
+        """
+        projectId = 6
+        if fromDate is None:
+            fromdate = 
+
+        sql = """
+    select 
+        pm.project_message_id,
+        pm.message_type,
+        pm.message,
+        pm.created_datetime,
+        u.user_id,
+        u.first_name,
+        u.last_name,
+        u.image_id,
+        g.user_group_id,
+        g.group_name,
+        i.idea_id,
+        i.description as idea_description,
+        i.submission_type as idea_submission_type,
+        i.created_datetime as idea_created_datetime
+        from project_message pm
+        inner join user u on u.user_id = pm.user_id
+        join user__user_group ug on ug.user_id = u.user_id
+        join user_group g on g.user_group_id = ug.user_group_id
+        left join idea i on i.idea_id = pm.idea_id
+        where pm.project_id = %(id)s and pm.is_active = 1
+        and (%(filterBy)s is null or pm.message_type = %(filterBy)s)
+        and i.created_datetime > %(from)s
+        order by g.user_group_id, pm.created_datetime desc
+    """
+
+        # cursor = self.DBHandle.cursor()
+        comments = self.executeSQL(sql, {'id':projectId, 'from':fromDate, 'filterBy':filterBy})
+        groups = {}
+        for comment in comments:
+            if not groups.get(comment.user_group_id):
+                groups[comment.user_group_id] = []
+            groups[comment.user_group_id].append(comment)
+        
+        return groups
+ 
+    def getRecentUsers(self, fromDate=None):
+        """
+        from: datetime of last digest
+        """
+
+        if fromDate is None:
+            fromDate = datetime.now() + relativedelta(days=-1)
+
+        sql = """
+    select 
     u.user_id,
     u.first_name,
     u.last_name,
     u.image_id,
+    u.created_datetime,
     g.user_group_id,
-    g.group_name,
-    i.idea_id,
-    i.description as idea_description,
-    i.submission_type as idea_submission_type,
-    i.created_datetime as idea_created_datetime
-    from project_message pm
-    inner join user u on u.user_id = pm.user_id
+    g.group_name
+    from user u 
     join user__user_group ug on ug.user_id = u.user_id
     join user_group g on g.user_group_id = ug.user_group_id
-    left join idea i on i.idea_id = pm.idea_id
-    where pm.project_id = $projectId and pm.is_active = 1
-    and ($filterBy is null or pm.message_type = $filterBy)
-    and i.created_datetime > $from
-    order by g.user_group_id, pm.created_datetime desc
-"""
-
-    comments = list(db.query(sql, {'id':projectId, 'from':fromDate, 'filterBy':filterBy}))
-    groups = {}
-    for comment in comments:
-        groups[comment.user_group_id].append(comment)
-    
-    return groups
-
-def get_recent_users(db, fromDate=None)
+    where u.created_datetime >= $fromDate
+    order by g.user_group_id, u.created_datetime desc
     """
-    from: datetime of last digest
-    """
+        users = self.executeSQL(sql, params = {'fromDate':fromDate})
+        groups = {}
+        for user in users:
+            if not groups.get(comment.user_group_id):
+                groups[user.user_group_id] = []
+            groups[user.user_group_id].append(user)
 
-    messages = []
-    if (filterBy not in ['member_comment','admin_comment','goal_achieved','join','endorsement']):
-        filterBy = None
+        return groups
 
-    sql = """
-select 
-u.user_id,
-u.first_name,
-u.last_name,
-u.image_id,
-u.created_datetime,
-g.user_group_id,
-g.group_name
-from user u 
-join user__user_group ug on ug.user_id = u.user_id
-join user_group g on g.user_group_id = ug.user_group_id
-where u.created_datetime >= $from
-order by g.user_group_id, u.created_datetime desc
-"""
-    users = list(db.query(sql, {'from':fromdate}))
-    groups = {}
-    for user in users:
-        groups[user.user_group_id].append(user)
+    def getDataToCreateDigest(self, fromDate=None):
+        users_by_group = self.getRecentUsers(fromDate=fromDate)
 
-    return groups
+        projectmessages = {}
+        projects = self.getProjects(fromDate=fromDate)
+        for project in projects:
+            messages_by_group = self.getRecentMessages(projectId=project, fromDate=fromDate)
+            print messages_by_group
+            
+            project_messages[project].append(messages_by_group)
 
-def create_digest(db, fromDate=None):
-    users_by_group = get_recent_users(db=db, fromDate=fromDate)
+        # Uniquify the list
+        groups = dict((x[0], x) for x in groups).values()
 
-    projectmessages = {}
-    for project in projects:
-        messages_by_group = get_recent_messages(db=db, projectId=project, fromDate=fromDate)
-        
-        project_messages[project].append(messages_by_group)
+        # Create the message
+        for group in groups:
+            pass
+            # Create the digest()
 
-    # Uniquify the list
-    groups = dict((x[0], x) for x in groups).values()
+    def getProjects(self):
+        sql = """
+        select distinct pm.project_id
+        from project_message pm
+        where pm.created_datetime > $fromDate
+        order by pm.project_id
+        """
+        projects = self.executeSQL(sql, params = {'fromDate':fromDate})
+        return projects
 
-    # Create the message
-    for group in gruops:
-        # Create the digest()
+# End class definition
+
+gamDigest = GiveAMinuteDigest(configFile="/Users/sundar/Projects/LP/gam2/branches/webpyUpgrade/config.yaml")
+gamDigest.getDataToCreateDigest()
