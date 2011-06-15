ee8850ec0b1becdedd337863adce6bb5c92b3f0b
sraman <sraman@befef031-aaeb-48d0-b70f-681328309d3d>
2011-06-15 04:55:05 +0000

    WIP: digest emailer; adds basic formatted output
    git-svn-id: http://svn.localprojects.net/gam2@1276 befef031-aaeb-48d0-b70f-681328309d3d

--- trunk/scripts/digest_emailer.py
+++ trunk/scripts/digest_emailer.py
@@ -32,7 +32,6 @@ class Mailable():
 
         self.SESSendQuota = self.SESHandle.get_send_quota()["GetSendQuotaResponse"]["GetSendQuotaResult"]
         # Check if we're close to the smtp quota. 10 seems like a good number
-
         sentLast24Hours = self.SESSendQuota.get('SentLast24Hours')
         if sentLast24Hours is None:
             sentLast24Hours = 0
@@ -101,29 +100,29 @@ class GiveAMinuteDigest(Configurable, WebpyDBConnectable, Mailable):
             raise "Cannot continue wihthout an explicit FromDate!"
 
         sql = """
-    select 
-        pm.project_message_id,
-        pm.project_id,
-        pm.message_type,
-        pm.message,
-        pm.created_datetime,
-        u.user_id,
-        u.first_name,
-        u.last_name,
-        u.image_id,
-        u.email,
-        i.idea_id,
-        i.description as idea_description,
-        i.submission_type as idea_submission_type,
-        i.created_datetime as idea_created_datetime
-        from project_message pm
-        inner join user u on u.user_id = pm.user_id
-        left join idea i on i.idea_id = pm.idea_id
-        where pm.project_id = $id and pm.is_active = 1
-        and ($filterBy is null or pm.message_type = $filterBy)
-        and pm.created_datetime > $fromDate
-        order by pm.created_datetime desc
-    """
+select 
+    pm.project_message_id,
+    pm.project_id,
+    pm.message_type,
+    pm.message,
+    pm.created_datetime,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.image_id,
+    u.email,
+    i.idea_id,
+    i.description as idea_description,
+    i.submission_type as idea_submission_type,
+    i.created_datetime as idea_created_datetime
+from project_message pm
+inner join user u on u.user_id = pm.user_id
+left join idea i on i.idea_id = pm.idea_id
+where pm.project_id = $id and pm.is_active = 1
+    and ($filterBy is null or pm.message_type = $filterBy)
+    and pm.created_datetime > $fromDate
+order by pm.created_datetime desc
+"""
         # TODO:
         # Should we be ordering/grouping by something other than the creationtime?
 
@@ -132,8 +131,8 @@ class GiveAMinuteDigest(Configurable, WebpyDBConnectable, Mailable):
         groups = {}
         for comment in comments:
             if not groups.get(comment.project_id):
-                groups[comment.user_group_id] = []
-            groups[comment.user_group_id].append(comment)
+                groups[comment.project_id] = []
+            groups[comment.project_id].append(comment)
         
         if len(groups.keys()) == 0:
             return False
@@ -149,44 +148,53 @@ class GiveAMinuteDigest(Configurable, WebpyDBConnectable, Mailable):
                         raise "Cannot continue wihthout an explicit FromDate!"
 
         sql = """
-    select 
+select 
     u.user_id,
     u.first_name,
     u.last_name,
     u.image_id,
     u.email_notification,
     u.created_datetime,
-    pu.project_id,
-    from user u 
-    join project__user pu on u.user_id = pu.user_id
-    where u.created_datetime >= $fromDate
-    order by p.project_id, u.created_datetime desc
-    """
+    pu.project_id
+from user u 
+join project__user as pu on u.user_id = pu.user_id
+where u.created_datetime >= $fromDate
+order by pu.project_id, u.created_datetime desc
+"""
         users = self.executeSQL(sql, params = {'fromDate':fromDate})
-        groups = {}
+        projects = {}
         for user in users:
-            if not groups.get(user.user_group_id):
-                groups[user.user_group_id] = []
-            groups[user.user_group_id].append(user)
+            if not projects.get(user.project_id):
+                projects[user.project_id] = []
+            projects[user.project_id].append(user)
 
-        return groups
+        return projects
 
     def getDataToCreateDigest(self, fromDate=None):
-        # users_by_group = self.getRecentUsers(fromDate=fromDate)
+        users_by_project = self.getRecentUsers(fromDate=fromDate)
 
-        project_messages = {}
+        project_feed = {}
         projects = self.getProjects(fromDate=fromDate)
         for project in projects:
             projId = int(project.project_id)
             print "Will get messages for project %s" % projId
-            messages_by_group = self.getRecentMessages(projectId=projId, fromDate=fromDate)
-            
-            if messages_by_group:
-                if project_messages.get(projId) is None:
-                    project_messages[projId] = []
-                project_messages[projId].append(messages_by_group)
 
-        return {'users': users_by_group, 'messages' : project_messages}
+            if project_feed.get(projId) is None:
+                project_feed[projId] = {}
+
+            messages_by_project = self.getRecentMessages(projectId=projId, fromDate=fromDate)
+
+            if users_by_project.get(projId) is not None:
+                if project_feed[projId].get('users') is None:
+                    project_feed[projId]['users'] = []
+                project_feed[projId]['users'].append(users_by_project.get(projId))
+            
+            if messages_by_project:
+                if project_feed[projId].get('messages') is None:
+                    project_feed[projId]['messages'] = []
+                project_feed[projId]['messages'].append(messages_by_project.get(projId))
+
+        return project_feed
 
         # Uniquify the list
         # groups = dict((x[0], x) for x in groups).values()
@@ -198,11 +206,24 @@ class GiveAMinuteDigest(Configurable, WebpyDBConnectable, Mailable):
 
     def createDigest(self):
         fromDate = datetime.now() + relativedelta(days=-31)
-
+        
         resp = self.getDataToCreateDigest(fromDate=fromDate)
-        print resp.get('users')
-        print resp.get('messages')
-
+        for projId in resp.keys():
+            body = ""
+            if resp[projId].get('users') is not None and len(resp[projId].get('users')) > 0:
+                body += "Users:\n"
+                for user in resp[projId].get('users'):
+                    body += ("<a href='http://link-to-user-profile'>%s %s</a>" % (user[0].first_name, user[0].last_name)) + "\n" 
+            
+            if resp[projId].get('messages') is not None and len(resp[projId].get('messages')) > 0:
+                body += "Messages:\n"
+                for message in resp[projId].get('messages'):
+                    body += message[0].get('message')
+                
+            print "Messages for Project %s: ..." % projId
+            print body
+            
+            
     def getProjects(self, fromDate=None):
         sql = """
         select distinct pm.project_id
