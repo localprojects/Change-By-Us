69ec47cff38c28ee8f3948ab2e7df06a6802b984
Andrew Mahon <andrew@typeslashcode.com>
2011-06-20 20:24:19 +0000

    fixed logout, added a new step to facebook login process,
    git-svn-id: http://svn.localprojects.net/gam2/trunk@1371 befef031-aaeb-48d0-b70f-681328309d3d

--- static/js/lp.gam.facebookConnect.js
+++ static/js/lp.gam.facebookConnect.js
@@ -6,11 +6,11 @@
 			FB.login(
 				function(response) {
 					if(response.session){
-						//window.location.href = "/login_facebook"
+						window.location.hash = 'facebook-login-preprocess';
 						window.location.href = "/facebook/login?uid="+response.session.uid+"&access_token="+response.session.access_token;
-						// window.location.href = "/login_facebook?uid="+response.session.uid+"&access_token="+response.session.access_token;
 					} else {
-						// login failure, do something here
+						tc.util.dump('LOGIN CANCELLED!');
+						tc.jQ('.cannot-be-clicked, .has-been-clicked').removeClass('has-been-clicked').removeClass('cannot-be-clicked');
 					}
 				},
 				{perms: requiredPerms.join(',')}
