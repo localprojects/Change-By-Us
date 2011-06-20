e21864006cfec449f79968f0e9886819b70c7e7d
Andrew Mahon <andrew@typeslashcode.com>
2011-06-20 20:44:09 +0000

    fixes r1785 Cancelling Facebook login disables all external login sources
    git-svn-id: http://svn.localprojects.net/gam2/trunk@1373 befef031-aaeb-48d0-b70f-681328309d3d

--- static/js/lp.gam.facebookConnect.js
+++ static/js/lp.gam.facebookConnect.js
@@ -7,9 +7,12 @@
 				function(response) {
 					if(response.session){
 						window.location.hash = 'facebook-login-preprocess';
+						
+						//POST will not work because we need to populate template 
+						// specific data related to this request.
 						window.location.href = "/facebook/login?uid="+response.session.uid+"&access_token="+response.session.access_token;
+						
 					} else {
-						tc.util.dump('LOGIN CANCELLED!');
 						tc.jQ('.cannot-be-clicked, .has-been-clicked').removeClass('has-been-clicked').removeClass('cannot-be-clicked');
 					}
 				},
