ef6192aac7ba07eeaf114cc9d5b8ca9b8067ad1c
Alan Palazzolo <github@zzolo.org>
2011-06-21 15:41:00 -0700

    Added new version of models.sql as we were not able to import via PHPMyAdmin or command line.

---
+++ sql/cfa-models.sql
@@ -0,0 +1,676 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jun 21, 2011 at 03:38 PM
+-- Server version: 5.1.44
+-- PHP Version: 5.3.2
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `cbu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `badwords`
+--
+
+DROP TABLE IF EXISTS `badwords`;
+CREATE TABLE IF NOT EXISTS `badwords` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kill_words` text,
+  `warn_words` text,
+  `updated_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `badwords`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `beta_invite_code`
+--
+
+DROP TABLE IF EXISTS `beta_invite_code`;
+CREATE TABLE IF NOT EXISTS `beta_invite_code` (
+  `code` char(10) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `created_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`code`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `beta_invite_code`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `beta_invite_request`
+--
+
+DROP TABLE IF EXISTS `beta_invite_request`;
+CREATE TABLE IF NOT EXISTS `beta_invite_request` (
+  `beta_invite_request_id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `comment` text,
+  `created_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`beta_invite_request_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `beta_invite_request`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `direct_message`
+--
+
+DROP TABLE IF EXISTS `direct_message`;
+CREATE TABLE IF NOT EXISTS `direct_message` (
+  `direct_message_id` int(11) NOT NULL AUTO_INCREMENT,
+  `to_user_id` int(11) NOT NULL,
+  `from_user_id` int(11) NOT NULL,
+  `message` text,
+  `created_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`direct_message_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `direct_message`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `facebook_user`
+--
+
+DROP TABLE IF EXISTS `facebook_user`;
+CREATE TABLE IF NOT EXISTS `facebook_user` (
+  `facebook_id` bigint(20) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `facebook_id` (`facebook_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `facebook_user`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `featured_project`
+--
+
+DROP TABLE IF EXISTS `featured_project`;
+CREATE TABLE IF NOT EXISTS `featured_project` (
+  `ordinal` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `updated_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ordinal`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `featured_project`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `idea`
+--
+
+DROP TABLE IF EXISTS `idea`;
+CREATE TABLE IF NOT EXISTS `idea` (
+  `idea_id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) NOT NULL,
+  `location_id` int(11) NOT NULL DEFAULT '-1',
+  `submission_type` enum('web','sms','email') NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phone` varchar(10) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `num_flags` smallint(6) NOT NULL DEFAULT '0',
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `created_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idea_id`),
+  FULLTEXT KEY `description` (`description`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `idea`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+CREATE TABLE IF NOT EXISTS `images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `app` varchar(255) NOT NULL,
+  `mirrored` tinyint(1) DEFAULT '0',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `app` (`app`),
+  KEY `mirrored` (`mirrored`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `images`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keyword`
+--
+
+DROP TABLE IF EXISTS `keyword`;
+CREATE TABLE IF NOT EXISTS `keyword` (
+  `keyword` varchar(25) NOT NULL,
+  `created_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`keyword`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `keyword`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location`
+--
+
+DROP TABLE IF EXISTS `location`;
+CREATE TABLE IF NOT EXISTS `location` (
+  `location_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `lat` decimal(9,6) NOT NULL,
+  `lon` decimal(9,6) NOT NULL,
+  `borough` varchar(50) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `state` char(2) DEFAULT NULL,
+  PRIMARY KEY (`location_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `location`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
+--
+
+DROP TABLE IF EXISTS `project`;
+CREATE TABLE IF NOT EXISTS `project` (
+  `project_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `image_id` int(11) DEFAULT NULL,
+  `location_id` int(11) DEFAULT NULL,
+  `keywords` text,
+  `num_flags` smallint(6) NOT NULL DEFAULT '0',
+  `is_official` tinyint(1) NOT NULL DEFAULT '0',
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `created_datetime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`project_id`),
+  FULLTEXT KEY `title` (`title`,`description`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `project`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_endorsement`
+--
+
+DROP TABLE IF EXISTS `project_endorsement`;
+CREATE TABLE IF NOT EXISTS `project_endorsement` (
+  `project_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`project_id`,`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `project_endorsement`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_goal`
+--
+
+DROP TABLE IF EXISTS `project_goal`;
+CREATE TABLE IF NOT EXISTS `project_goal` (
+  `project_goal_id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `time_frame_numeric` int(11) DEFAULT NULL,
+  `time_frame_unit` enum('day','week','month') DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `is_accomplished` tinyint(1) NOT NULL DEFAULT '0',
+  `num_flags` smallint(6) NOT NULL DEFAULT '0',
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `created_datetime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`project_goal_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `project_goal`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_invite`
+--
+
+DROP TABLE IF EXISTS `project_invite`;
+CREATE TABLE IF NOT EXISTS `project_invite` (
+  `project_invite_id` int(11) NOT NULL AUTO_INCREMENT,
+  `message` varchar(255) DEFAULT NULL,
+  `project_id` int(11) NOT NULL,
+  `inviter_user_id` int(11) NOT NULL,
+  `invitee_idea_id` int(11) DEFAULT NULL,
+  `invitee_user_id` int(11) DEFAULT NULL,
+  `invitee_email` int(11) DEFAULT NULL,
+  `accepted_datetime` timestamp NULL DEFAULT NULL,
+  `created_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`project_invite_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `project_invite`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_leader`
+--
+
+DROP TABLE IF EXISTS `project_leader`;
+CREATE TABLE IF NOT EXISTS `project_leader` (
+  `user_id` int(11) NOT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `organization` varchar(255) DEFAULT NULL,
+  `created_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `project_leader`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_link`
+--
+
+DROP TABLE IF EXISTS `project_link`;
+CREATE TABLE IF NOT EXISTS `project_link` (
+  `project_link_id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `image_id` int(11) DEFAULT NULL,
+  `num_flags` smallint(6) NOT NULL DEFAULT '0',
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `created_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`project_link_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `project_link`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_message`
+--
+
+DROP TABLE IF EXISTS `project_message`;
+CREATE TABLE IF NOT EXISTS `project_message` (
+  `project_message_id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` int(11) NOT NULL,
+  `message` varchar(255) DEFAULT NULL,
+  `message_type` enum('member_comment','admin_comment','goal_achieved','join','endorsement') DEFAULT NULL,
+  `idea_id` int(11) DEFAULT NULL,
+  `project_goal_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `num_flags` smallint(6) NOT NULL DEFAULT '0',
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `created_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`project_message_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `project_message`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_resource`
+--
+
+DROP TABLE IF EXISTS `project_resource`;
+CREATE TABLE IF NOT EXISTS `project_resource` (
+  `project_resource_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `twitter_url` varchar(255) DEFAULT NULL,
+  `facebook_url` varchar(255) DEFAULT NULL,
+  `physical_address` varchar(255) DEFAULT NULL,
+  `contact_name` varchar(255) DEFAULT NULL,
+  `contact_email` varchar(100) DEFAULT NULL,
+  `contact_user_id` int(11) DEFAULT NULL,
+  `image_id` int(11) DEFAULT NULL,
+  `location_id` int(11) DEFAULT NULL,
+  `keywords` text,
+  `is_hidden` tinyint(1) NOT NULL DEFAULT '0',
+  `is_official` tinyint(1) NOT NULL DEFAULT '0',
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `created_datetime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`project_resource_id`),
+  FULLTEXT KEY `title` (`title`,`description`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `project_resource`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project__idea`
+--
+
+DROP TABLE IF EXISTS `project__idea`;
+CREATE TABLE IF NOT EXISTS `project__idea` (
+  `project_id` int(11) NOT NULL,
+  `idea_id` int(11) NOT NULL,
+  `created_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`project_id`,`idea_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `project__idea`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project__project_resource`
+--
+
+DROP TABLE IF EXISTS `project__project_resource`;
+CREATE TABLE IF NOT EXISTS `project__project_resource` (
+  `project_id` int(11) NOT NULL,
+  `project_resource_id` int(11) NOT NULL,
+  `created_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`project_id`,`project_resource_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `project__project_resource`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project__user`
+--
+
+DROP TABLE IF EXISTS `project__user`;
+CREATE TABLE IF NOT EXISTS `project__user` (
+  `project_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `is_project_admin` tinyint(1) NOT NULL DEFAULT '0',
+  `created_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`project_id`,`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `project__user`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `site_feedback`
+--
+
+DROP TABLE IF EXISTS `site_feedback`;
+CREATE TABLE IF NOT EXISTS `site_feedback` (
+  `site_feedback_id` int(11) NOT NULL AUTO_INCREMENT,
+  `submitter_name` varchar(100) DEFAULT NULL,
+  `submitter_email` varchar(100) DEFAULT NULL,
+  `comment` text,
+  `is_responded` tinyint(1) NOT NULL DEFAULT '0',
+  `responded_user_id` int(11) DEFAULT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `created_datetime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`site_feedback_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `site_feedback`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sms_stopped_phone`
+--
+
+DROP TABLE IF EXISTS `sms_stopped_phone`;
+CREATE TABLE IF NOT EXISTS `sms_stopped_phone` (
+  `phone` varchar(10) NOT NULL,
+  `created_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`phone`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sms_stopped_phone`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `twitter_user`
+--
+
+DROP TABLE IF EXISTS `twitter_user`;
+CREATE TABLE IF NOT EXISTS `twitter_user` (
+  `twitter_id` int(11) NOT NULL,
+  `twitter_username` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `twitter_id` (`twitter_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `twitter_user`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unauthenticated_user`
+--
+
+DROP TABLE IF EXISTS `unauthenticated_user`;
+CREATE TABLE IF NOT EXISTS `unauthenticated_user` (
+  `auth_guid` char(36) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `salt` varchar(255) NOT NULL,
+  `phone` varchar(10) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `created_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`auth_guid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `unauthenticated_user`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_key` varchar(10) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `salt` varchar(255) NOT NULL,
+  `phone` varchar(10) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `full_display_name` varchar(255) DEFAULT NULL,
+  `image_id` int(11) DEFAULT NULL,
+  `location_id` int(11) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `affiliation` varchar(100) DEFAULT NULL,
+  `is_oncall` tinyint(1) NOT NULL DEFAULT '0',
+  `email_notification` enum('none','digest') NOT NULL DEFAULT 'digest',
+  `last_account_page_access_datetime` timestamp NULL DEFAULT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `created_datetime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `phone` (`phone`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `user`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_group`
+--
+
+DROP TABLE IF EXISTS `user_group`;
+CREATE TABLE IF NOT EXISTS `user_group` (
+  `user_group_id` int(11) NOT NULL,
+  `group_name` varchar(50) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `created_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_group_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_group`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user__user_group`
+--
+
+DROP TABLE IF EXISTS `user__user_group`;
+CREATE TABLE IF NOT EXISTS `user__user_group` (
+  `user_id` int(11) NOT NULL,
+  `user_group_id` int(11) NOT NULL,
+  `created_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`,`user_group_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user__user_group`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `web_session`
+--
+
+DROP TABLE IF EXISTS `web_session`;
+CREATE TABLE IF NOT EXISTS `web_session` (
+  `session_id` char(128) NOT NULL,
+  `atime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `data` text,
+  UNIQUE KEY `session_id` (`session_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `web_session`
+--
+
