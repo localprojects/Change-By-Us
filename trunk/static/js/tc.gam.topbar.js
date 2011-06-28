c2a1f001c461b2d08c22b5d500e17fd0bbe7243c
ilord <ilord@befef031-aaeb-48d0-b70f-681328309d3d>
2011-06-28 21:30:16 +0000

    ipad fixes for dropdowns, button and nav padding, and more!
    git-svn-id: http://svn.localprojects.net/gam2@1468 befef031-aaeb-48d0-b70f-681328309d3d

--- trunk/static/js/tc.gam.topbar.js
+++ trunk/static/js/tc.gam.topbar.js
@@ -11,15 +11,27 @@ tc.top_bar = function(element, options) {
 	tc.jQ('div.dropdown').removeClass('no-js');
 	
 	function init() {
-		element.find(".username, .myprojects").mouseenter(function () {
-			if( $.browser.msie && $.browser.version < 8 ) {
-				tc.jQ(this).children(".dropdown").stop(true, true).fadeIn(o.slideSpeed);
-			} else {
-				tc.jQ(this).children(".dropdown").stop(true, true).slideDown(o.slideSpeed);
-			}
-		}).mouseleave(function () {
-			tc.jQ(this).children(".dropdown").fadeOut(o.fadeSpeed);
-		});
+		if (isiPad == true) {
+			element.find(".username > a, .myprojects > a").removeAttr('href');
+			element.find(".username > a, .myprojects > a").toggle(
+				function() { 
+					tc.jQ('.userland .dropdown').hide();
+					tc.jQ(this).parent().children(".dropdown").stop(true, true).slideDown(o.slideSpeed);
+				}, function() { 
+					tc.jQ(this).parent().children(".dropdown").fadeOut(o.fadeSpeed); 
+				}
+			);
+		} else {
+			element.find(".username, .myprojects").mouseenter(function () {
+				if( $.browser.msie && $.browser.version < 8 ) {
+					tc.jQ(this).children(".dropdown").stop(true, true).fadeIn(o.slideSpeed);
+				} else {
+					tc.jQ(this).children(".dropdown").stop(true, true).slideDown(o.slideSpeed);
+				}
+			}).mouseleave(function () {
+				tc.jQ(this).children(".dropdown").fadeOut(o.fadeSpeed);
+			});
+		};
 	}
 	
 	init();
