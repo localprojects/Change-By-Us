1d533b969035e8f109af39b2df88ba9112c64cb5
Aaron Ogle <atogle@roosevelt.local>
2011-06-29 14:29:57 -0700

    add safeuni test

--- tests/util-tests.py
+++ tests/util-tests.py
@@ -8,6 +8,22 @@ class UtilTests (unittest.TestCase):
         self.assertEqual(util.try_f(int, "foo", -1), -1, "Did not get given default value as expected.")
         self.assertEqual(util.try_f(int, "foo"), None, "Did not get None as expected.")
         self.assertEqual(util.try_f(str, 10, "ten"), "10", "Did not convert int to string.")
+
+    def test_safeuni(self):
+        class HasUnicode(object):
+            def __unicode__(self):
+                return u"unicode here"
         
+        uni = u"unicode"
+        base = "basestring"
+        hasUni = HasUnicode()
+        noUni = 5
+        
+        self.assertEqual(type(util.safeuni(uni)), unicode, "Did not return a unicode string from uni.")
+        self.assertEqual(type(util.safeuni(base)), unicode, "Did not return a unicode string from base.")
+        self.assertEqual(type(util.safeuni(hasUni)), unicode, "Did not return a unicode string from hasUni.")
+        self.assertEqual(util.safeuni(hasUni), "unicode here", "Did not return the expected string from hasUni.")
+        self.assertEqual(type(util.safeuni(noUni)), unicode, "Did not return a unicode string from noUni.")
+
 if __name__ == "__main__":
     unittest.main()
