933bdf767c80c9c5e6c5c9f1543a73b983f05797
Alan Palazzolo <github@zzolo.org>
2011-07-14 14:01:43 -0700

    Added some more basic tests for config testing.

--- tests/unittests/config-tests.py
+++ tests/unittests/config-tests.py
@@ -14,8 +14,39 @@ class ConfigTest (TestCase):
     
     def test_load(self):
         """
-        Test that a config has been loaded.
+        Test `load` method and that a config has been loaded.
+        
         """
         Config.load()
         self.assertIsNotNone(Config.data)
         self.assertIsInstance(Config.data, dict)
+    
+    def test_get_all(self):
+        """
+        Test `get_all` method
+        
+        """
+        data = Config.get_all()
+        self.assertIsNotNone(data)
+        self.assertIsInstance(data, dict)
+    
+    def test_dev(self):
+        """
+        Test `dev` method
+        
+        """
+        dev = Config.dev()
+        self.assertIsInstance(dev, bool)
+    
+    def test_manualSetConfig(self):
+        """
+        Test manually setting a value.
+        
+        """
+        # Assert that value is not there
+        self.assertNotIn('testing', Config.data)
+        
+        # Set new value and assert it is there
+        Config.data['testing'] = True
+        data = Config.get_all()
+        self.assertTrue(data['testing'])
