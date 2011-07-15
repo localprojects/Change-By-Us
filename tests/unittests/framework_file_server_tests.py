e72a29c6b08848ea7d040e60af9b4e03eb448cb8
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2011-07-15 11:26:52 -0700

    Change the file_server test to use Mock

--- tests/unittests/framework_file_server_tests.py
+++ tests/unittests/framework_file_server_tests.py
@@ -1,16 +1,21 @@
 from unittest import TestCase
 from lib import web
+from mock import Mock
 
 import main
 import framework.file_server as file_server
 
 class BaseFileServerTests (TestCase):
 
-    def test_AddShoulfReturnNoneIfDbInsertionFails(self):
+    def test_AddShouldReturnNoneIfDbInsertionFails(self):
         fs = file_server.FileServer()
+        fs._addDbRecord = Mock(return_value=None)
+        fs._saveFile = Mock()
         
         db = main.sessionDB()
-        db.insert = lambda *_: None
         id = fs.add(db, "This is file data", "myapp")
         
+        self.assertEqual(fs._addDbRecord.call_count, 1)
+        self.assertFalse(fs._saveFile.called)
         self.assertIsNone(id)
+
