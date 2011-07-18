3fcf7ef0dc6c0d61fe78d194714ad32222acfa1d
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2011-07-17 17:50:17 -0700

    Mock removeDbRecord so that the database cannot be affected in the test for S3 uploading.

--- tests/integrationtests/framework_file_server_tests.py
+++ tests/integrationtests/framework_file_server_tests.py
@@ -9,11 +9,15 @@ class S3FileServerTests (TestCase):
     
     def test_S3Upload(self):
         fs = file_server.S3FileServer()
+        
+        # This should test S3 uploading, not the DB.
         fs.addDbRecord = Mock(return_value=3)
+        fs.removeDbRecord = Mock()
         
         db = main.sessionDB()
         id = fs.add(db, "This is file data", "myapp")
         
         self.assertEqual(fs.addDbRecord.call_count, 1)
+        self.assertEqual(fs.removeDbRecord.call_count, 0)
         self.assertEqual(id, 3)
 
