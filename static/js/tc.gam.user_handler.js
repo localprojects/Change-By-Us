4b3d369059e482de7fdcc53cc46356c7dcc6f4ab
Alan Palazzolo <github@zzolo.org>
2011-07-27 15:48:07 -0700

    Added documentation and formatted user handler js file.

--- static/js/tc.gam.user_handler.js
+++ static/js/tc.gam.user_handler.js
@@ -1,21 +1,42 @@
-if(!tc){ var tc = {}; }
+/**
+ * File: User Handler
+ * User handling functionality.
+ * 
+ * Dependencies:
+ * - tc.gam.base.js
+ * - tc.utils.js
+ */
 
-tc.user_handler = function(options){
-	return function(app){
-		tc.util.log('tc.user_handler');
-		if(app.app_page.user){
-			if(typeof options.user_handler == 'function'){
-				if(options.user_handler(app) === false){
-					return false;
-				}
-			}
-		} else {
-			if(typeof options.no_user_handler == 'function'){
-				if(options.no_user_handler(app) === false){
-					return false;
-				}
-			}
-		}
-		return true;
-	};
+/**
+ * Function: tc.user_handler
+ * Returns function that takes in an App, determins if there is user 
+ * (app.app_page.user) and runs either options handler appropriately,
+ * returning boolean.
+ *
+ * Parameters:
+ * options - {Object} An object that has a the following functions
+ *     - user_handler: Handles when there is a user present.
+ *     - no_user_handler: Handles when there is no user.
+ *
+ * Returns:
+ * {Function} A function that takes in the global "app" parameter and
+ * returns a boolean based on user handler function.
+ */
+tc.user_handler = function(options) {
+    return function(app) {
+        if (app.app_page.user) {
+            if (typeof options.user_handler == 'function') {
+                if (options.user_handler(app) === false) {
+                    return false;
+                }
+            }
+        } else {
+            if (typeof options.no_user_handler == 'function') {
+                if (options.no_user_handler(app) === false) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    };
 }
