970cee2c1188b102499d2303a829a3939efdd0a5
Alan Palazzolo <github@zzolo.org>
2011-07-29 18:11:28 -0700

    More documentation for JS unittests.

--- tests/js/unittests/tc.gam.validate.spec.js
+++ tests/js/unittests/tc.gam.validate.spec.js
@@ -11,6 +11,10 @@ EnvJasmine.load(EnvJasmine.jsDir + 'tc.gam.validate.js');
  * Tests: GAM Validate Tests
  */
 describe('GAM Validate', function () {
+
+    /**
+     * Test Function: tc.countContain
+     */
     it('finds two capital letters', function () {
         var c = tc.countContain('Code for America', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
         expect(c).toEqual(2);
