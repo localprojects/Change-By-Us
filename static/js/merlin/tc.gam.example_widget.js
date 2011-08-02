a7202ae30286e9c0d536a927a974126c6e4e0095
Alan Palazzolo <github@zzolo.org>
2011-08-01 17:14:20 -0700

    Adding more logic merlin example.

--- static/js/merlin/tc.gam.example_widget.js
+++ static/js/merlin/tc.gam.example_widget.js
@@ -27,25 +27,35 @@ tc.gam.widgets.example_widget = function(dom) {
         name: 'merlin_app_id',
         dom: tc.jQ('.merlin.example-wizard'),
         next_button: tc.jQ('a.next-button'),
-        back_button: tc.jQ('a.back-button'),
+        back_button: tc.jQ('a.back-button'),  // Still not working or fully understood
         error_indicator: tc.jQ('.error-indicator'),
+        progress_element: tc.jQ('.progress-element'),  // Still not working or fully understood
+        magic: false, // Makes a mess
         first_step: 'step_id_1',
         data: {
             data_property: 'default-value',
             text_input: '',
             checkbox_input: '0',
-            email_input: ''
+            email_input: '',
+            error_select: 'none'
         },
         use_hashchange: false,
         steps: {
             'step_id_1': {
+                progress_selector: '.1',  // Still not working or fully understood
                 selector: '.step.step-selector-1',
+                prev_step: null,
                 next_step: 'step_id_2',
+				use_for_history: false, // Not sure what is used for.
                 inputs: {
                     'input_textfield': {
                         selector: 'input.textfield-input',
                         validators: ['min-3', 'max-200', 'required'],
                         hint: 'This is a hint for a textfield...',
+                        counter: {
+                            selector: '.charlimit.charlimit-textfield-input',
+                            limit: 200
+                        },
                         handlers: {
                             focus: function(e, d) {
                                 tc.jQ('.messages').append('<li>Focuses on the input field.</li>');
@@ -71,13 +81,18 @@ tc.gam.widgets.example_widget = function(dom) {
                 }
             },
             'step_id_2': {
+                progress_selector: '.2', // Still not working or fully understood
                 selector: '.step.step-selector-2',
+                prev_step: 'step_id_1',
                 next_step: 'step_id_3_success',
                 inputs: {
                     'input_textfield_email': {
                         selector: 'input.textfield-input-email',
                         validators: ['email', 'required'],
                         hint: 'Please enter your email'
+                    },
+                    'error_select': {
+                        selector: 'select.select-error'
                     }
                 },
                 init: function(merlin, dom) {
@@ -86,23 +101,36 @@ tc.gam.widgets.example_widget = function(dom) {
                 finish: function(merlin, dom) {
                     // Add data from the inputs.
                     merlin.options.data = tc.jQ.extend(merlin.options.data, {
-                        email_input: merlin.current_step.inputs.input_textfield_email.dom.val()
+                        email_input: merlin.current_step.inputs.input_textfield_email.dom.val(),
+                        error_select: merlin.current_step.inputs.error_select.dom.val()
                     });
+                    
+                    tc.jQ('.messages').append('<li>Passed Step 02.</li>');
+                    tc.jQ('.messages').append('<li>Data: ' + JSON.stringify(merlin.options.data) + '</li>');
+                    
+                    if (merlin.options.data.error_select == 'error') {
+                        // How to raise error in merline?
+                        tc.jQ('.messages').append('<li>Raising error.</li>');
+                    }
+                    // This causes recursion
+                    /*
+                    if (merlin.options.data.error_select == 'redirect') {
+                        merlin.show_step('step_id_1');
+                        tc.jQ('.messages').append('<li>Redirecting.</li>');
+                    }
+                    */
                 }
             },
             'step_id_3_success': {
                 selector: '.step.step-id-3-success',
                 init: function(merlin, dom) {
-                    alert('Success!  Data: ' + merlin.options.data);
+                    tc.jQ('.messages').append('<li>Init Step 03, Success.</li>');
                 }
             },
             'step_id_4_error': {
                 selector: '.step.step-id-4-error',
                 init: function(merlin, dom) {
-                    alert('There was an error');
-                    
-                    // Go back to first step
-                    merlin.show_step('step_id_1');
+                    tc.jQ('.messages').append('<li>Init Step 04, Error.</li>');
                 }
             }
         }
