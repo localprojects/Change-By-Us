1a46774d704d8016c2daba1dfad536cb05b32e2a
Alan Palazzolo <github@zzolo.org>
2011-08-02 17:54:30 -0700

    Added inline documentation and formatting changes.

--- static/js/tc.gam.project.js
+++ static/js/tc.gam.project.js
@@ -1,40 +1,74 @@
-if(!tc){ var tc = {}; }
-if(!tc.gam){ tc.gam = {}; }
+/**
+ * File: Project
+ * This file ....
+ *
+ * Filename:
+ * tc.game.project.js
+ * 
+ * Dependencies:
+ * - tc.gam.base.js
+ * - tc.utils.js
+ * - tc.gam.app.js
+ */
 
-
-tc.gam.project = function(options){    
-    var me, hash_onload;
+/**
+ * Function: tc.gam.project
+ * Project object for  ....
+ *
+ * Parameters:
+ * options - {Object} Object of options
+ *
+ * Returns:
+ * {Object} ??
+ */
+tc.gam.project = function(options) {    
+    var me;
+    var hash_onload;
     me = this;
-    tc.util.log("tc.gam.project");
+    this_project = this;
     
-    this.options = tc.jQ.extend({
-        
-    },options);
+    // Combine options (this is currently unnecessary).
+    this.options = tc.jQ.extend({}, options);
     
+    // Define local properties.
     this.dom = this.options.dom;
     this.event_data = { project:this };
     this.data = this.options.data;
     this.widget = new tc.gam.widget(null,this);
     
+    // Get hash location.
     hash_onload = window.location.hash;
     
+    // Components of the project interface.
     this.components = {
-        infopane:new tc.gam.project_widgets.infopane(this,this.dom.find('.box.mission'),{widget:this.widget},{app:options.app}),
-        resources:new tc.gam.project_widgets.resources(this,this.dom.find('.box.resources'),{widget:this.widget},{app:options.app}),
-        related_resources:new tc.gam.project_widgets.related_resources(this,this.dom.find('.box.related-resources'),{widget:this.widget},{app:options.app}),
-        add_link:new tc.gam.project_widgets.add_link(this,this.dom.find('.box.add-link'),{widget:this.widget},{app:options.app}),
-        goals_main:new tc.gam.project_widgets.goals_main(this,this.dom.find('.box.goals-main'),{widget:this.widget},{app:options.app}),
-        goals_add:new tc.gam.project_widgets.goals_add(this,this.dom.find('.box.goals-add'),{widget:this.widget},{app:options.app}),
-        conversation:new tc.gam.project_widgets.conversation(this,this.dom.find('.box.conversation'),{widget:this.widget},{app:options.app}),
-        members:new tc.gam.project_widgets.members(this,this.dom.find('.box.members'),{widget:this.widget},{app:options.app})
+        'infopane': new tc.gam.project_widgets.infopane(this, this.dom.find('.box.mission'),
+            { widget: this.widget }, { app: options.app }),
+        'resources': new tc.gam.project_widgets.resources(this, this.dom.find('.box.resources'),
+            { widget: this.widget }, { app: options.app }),
+        'related_resources': new tc.gam.project_widgets.related_resources(this, this.dom.find('.box.related-resources'),
+            { widget: this.widget }, { app: options.app }),
+        'add_link': new tc.gam.project_widgets.add_link(this, this.dom.find('.box.add-link'),
+            { widget: this.widget }, { app: options.app }),
+        'goals_main': new tc.gam.project_widgets.goals_main(this, this.dom.find('.box.goals-main'),
+            { widget: this.widget }, { app: options.app }),
+        'goals_add': new tc.gam.project_widgets.goals_add(this, this.dom.find('.box.goals-add'),
+            { widget: this.widget }, { app: options.app }),
+        'conversation': new tc.gam.project_widgets.conversation(this, this.dom.find('.box.conversation'),
+            { widget: this.widget }, { app: options.app }),
+        'members': new tc.gam.project_widgets.members(this, this.dom.find('.box.members'),
+            { widget: this.widget }, { app: options.app })
     };
-        
+    
+    // Add fresh ideas component if available.
     if (tc.gam.project_widgets.fresh_ideas) {
-        this.components.related_ideas = new tc.gam.project_widgets.fresh_ideas(this,this.dom.find('.box.fresh-ideas'),{widget:this.widget},{app:options.app});
+        this.components.related_ideas = new tc.gam.project_widgets.fresh_ideas(this, 
+            this.dom.find('.box.fresh-ideas'), { widget: this.widget }, { app: options.app });
     }
     
-    if(tc.gam.project_widgets.goals_stack){
-        this.components.goals_stack = new tc.gam.project_widgets.goals_stack(this,this.dom.find('.box.goals-stack-holder'),{widget:this.widget},{app:options.app});
+    // Add goals stack if available
+    if (tc.gam.project_widgets.goals_stack) {
+        this.components.goals_stack = new tc.gam.project_widgets.goals_stack(this,
+            this.dom.find('.box.goals-stack-holder'),{ widget: this.widget }, { app: options.app });
     }
     
     // return project page to initial state
