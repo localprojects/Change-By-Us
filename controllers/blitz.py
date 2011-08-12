ccec727aa6bb8607613aa37ed6b8f6a8f740c8f8
Alan Palazzolo <github@zzolo.org>
2011-08-12 15:47:27 -0700

    Added handling of custom route for blitz registration.

---
+++ controllers/blitz.py
@@ -0,0 +1,23 @@
+"""
+Module to handle integration with Bltiz.io.  The service
+requires that a specific route give back a specific value.
+This is important so that it is not used on sites
+unknowingly.
+
+"""
+import framework.util as util
+from framework.config import Config
+
+class Blitz():
+    """
+    Controller class to handle Bltiz.io routes.
+    
+    """
+    
+    def GET(self, action = None):
+        """
+        Get for Blitz.io route
+        
+        """
+        response = Config.get('blitz_io').get('response')
+        return response
