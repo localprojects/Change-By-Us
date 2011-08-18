1cb0d237ce2f30237650ee9cfa1bf00103cf05b6
Alan Palazzolo <github@zzolo.org>
2011-08-18 14:22:47 -0700

    Updated build type for models export command.

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,7 +18,7 @@ rm -r modules
 # Models.sql reminder
 echo "========= MODELING ============"
 echo "Ensure that the models.sql file is up to date with something similar to:"
-echo "mysqldump --no-data -u USER_NAME -p DB_NAME | sed 's/\(.*ENGINE.*AUTO_INCREMENT=\).*/\10;/g' > sql/models-temp.sql"
+echo "mysqldump --no-data -u USER_NAME -p DB_NAME | sed 's/\(.*ENGINE.*AUTO_INCREMENT=\).*/\10;/g' > sql/models.sql"
 echo ""
 echo "Also make sure migrations scripts are created."
 
