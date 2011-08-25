f035d01fbc07eeac8d77173e73e7092ec3f26052
Alan Palazzolo <github@zzolo.org>
2011-08-25 13:54:35 +0100

    Resource import sql script.

---
+++ import/import-resources.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 25, 2011 at 01:48 PM
+-- Server version: 5.1.44
+-- PHP Version: 5.3.2
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Database: `cbu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_resource`
+--
+
+DROP TABLE IF EXISTS `import_project_resource`;
+CREATE TABLE IF NOT EXISTS `import_project_resource` (
+  `project_resource_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `twitter_url` varchar(255) DEFAULT NULL,
+  `facebook_url` varchar(255) DEFAULT NULL,
+  `physical_address` varchar(255) DEFAULT NULL,
+  `contact_name` varchar(255) DEFAULT NULL,
+  `contact_email` varchar(100) DEFAULT NULL,
+  `contact_user_id` int(11) DEFAULT NULL,
+  `image_id` int(11) DEFAULT NULL,
+  `location_id` int(11) DEFAULT NULL,
+  `keywords` text,
+  `is_hidden` tinyint(1) NOT NULL DEFAULT '0',
+  `is_official` tinyint(1) NOT NULL DEFAULT '0',
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `created_datetime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `external_id` int(11) NOT NULL,
+  PRIMARY KEY (`project_resource_id`),
+  FULLTEXT KEY `title` (`title`,`description`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
