84631197262ecdef294ef73e58e74223eaeb35d1
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2011-08-26 18:40:35 -0700

    Add a serializer to the rest controller.  dates should be returned as strings now, so that they can be JSON serialized.

--- controllers/rest.py
+++ controllers/rest.py
@@ -1,9 +1,12 @@
+import inspect
 import json
 
 from lib import web
 
 from framework.controller import Controller
 from framework.log import log
+from framework.util import safestr
+from framework.util import safeuni
 
 from giveaminute import models
 
@@ -79,6 +82,273 @@ class NonProjectAdminReadOnly (ResourceAccessRules):
         return self.is_project_admin(user, instance.project_id)
 
 
+def _field_to_tuple(field):
+    """
+    Convert an item in the `fields` attribute into a 2-tuple. 
+    """
+    if isinstance(field, (tuple, list)):
+        return (field[0], field[1])
+    return (field, None)
+
+def _fields_to_list(fields):
+    """
+    Return a list of field names.
+    """
+    return [_field_to_tuple(field)[0] for field in fields or ()]
+
+def _fields_to_dict(fields):
+    """
+    Return a `dict` of field name -> None, or tuple of fields, or Serializer class
+    """
+    return dict([_field_to_tuple(field) for field in fields or ()])
+
+
+class Serializer(object):
+    """
+    Converts python objects into plain old native types suitable for
+    serialization.  In particular it handles models and querysets.
+    
+    The output format is specified by setting a number of attributes
+    on the class.
+
+    You may also override any of the serialization methods, to provide
+    for more flexible behavior.
+ 
+    Valid output types include anything that may be directly rendered into
+    json, xml etc...
+    """
+
+    fields = () 
+    """
+    Specify the fields to be serialized on a model or dict.
+    Overrides `include` and `exclude`.
+    """
+
+    include = ()
+    """
+    Fields to add to the default set to be serialized on a model/dict.
+    """
+
+    exclude = ()
+    """
+    Fields to remove from the default set to be serialized on a model/dict.
+    """
+
+    rename = {}
+    """
+    A dict of key->name to use for the field keys.
+    """
+
+    related_serializer = None
+    """
+    The default serializer class to use for any related models.
+    """
+
+    depth = None
+    """
+    The maximum depth to serialize to, or `None`.
+    """
+
+
+    def __init__(self, depth=None, stack=[], **kwargs):
+        self.depth = depth or self.depth
+        self.stack = stack
+        
+
+    def get_fields(self, obj):
+        """
+        Return the set of field names/keys to use for a model instance/dict.
+        """
+        fields = self.fields
+
+        # If `fields` is not set, we use the default fields and modify
+        # them with `include` and `exclude`
+        if not fields:
+            default = self.get_default_fields(obj)
+            include = self.include or ()
+            exclude = self.exclude or ()
+            fields = set(default + list(include)) - set(exclude)
+
+        else:
+            fields = _fields_to_list(self.fields)
+
+        return fields
+
+
+    def get_default_fields(self, obj):
+        """
+        Return the default list of field names/keys for a model instance/dict.
+        These are used if `fields` is not given.
+        """
+#        if isinstance(obj, models.Model):
+#            opts = obj._meta
+#            return [field.name for field in opts.fields + opts.many_to_many]
+#        else:
+        return obj.keys()
+
+
+    def get_related_serializer(self, key):
+        info = _fields_to_dict(self.fields).get(key, None)
+
+        # If an element in `fields` is a 2-tuple of (str, tuple)
+        # then the second element of the tuple is the fields to
+        # set on the related serializer
+        if isinstance(info, (list, tuple)):
+            class OnTheFlySerializer(Serializer):
+                fields = info
+            return OnTheFlySerializer
+
+        # If an element in `fields` is a 2-tuple of (str, Serializer)
+        # then the second element of the tuple is the Serializer
+        # class to use for that field.
+        elif isinstance(info, type) and issubclass(info, Serializer):
+            return info
+
+        # If an element in `fields` is a 2-tuple of (str, str)
+        # then the second element of the tuple is the name of the Serializer
+        # class to use for that field.
+        #
+        # Black magic to deal with cyclical Serializer dependancies.
+        # Similar to what Django does for cyclically related models.
+        elif isinstance(info, str) and info in _serializers:
+            return _serializers[info]
+        
+        # Otherwise use `related_serializer` or fall back to `Serializer`
+        return getattr(self, 'related_serializer') or Serializer
+
+
+    def serialize_key(self, key):
+        """
+        Keys serialize to their string value,
+        unless they exist in the `rename` dict.
+        """
+        return self.rename.get(safestr(key), safestr(key))
+
+
+    def serialize_val(self, key, obj):
+        """
+        Convert a model field or dict value into a serializable representation.
+        """
+        related_serializer = self.get_related_serializer(key)
+     
+        if self.depth is None:
+            depth = None
+        elif self.depth <= 0:
+            return self.serialize_max_depth(obj)
+        else:
+            depth = self.depth - 1
+
+        if any([obj is elem for elem in self.stack]):
+            return self.serialize_recursion(obj)
+        else:
+            stack = self.stack[:]
+            stack.append(obj)
+
+        return related_serializer(depth=depth, stack=stack).serialize(obj)
+
+
+    def serialize_max_depth(self, obj):
+        """
+        Determine how objects should be serialized once `depth` is exceeded.
+        The default behavior is to ignore the field.
+        """
+        raise _SkipField
+
+
+    def serialize_recursion(self, obj):
+        """
+        Determine how objects should be serialized if recursion occurs.
+        The default behavior is to ignore the field.
+        """
+        raise _SkipField
+
+
+    def serialize_model(self, instance):
+        """
+        Given a model instance or dict, serialize it to a dict..
+        """
+        data = {}
+
+        fields = self.get_fields(instance)
+
+        # serialize each required field 
+        for fname in fields:
+            if hasattr(self, safestr(fname)):
+                # check first for a method 'fname' on self first
+                meth = getattr(self, fname)
+                if inspect.ismethod(meth) and len(inspect.getargspec(meth)[0]) == 2:
+                    obj = meth(instance)
+            elif hasattr(instance, '__contains__') and fname in instance:
+                # check for a key 'fname' on the instance
+                obj = instance[fname]
+            elif hasattr(instance, safestr(fname)):
+                # finally check for an attribute 'fname' on the instance
+                obj = getattr(instance, fname)
+            else:
+                continue
+
+            key = self.serialize_key(fname)
+            val = self.serialize_val(fname, obj)
+            data[key] = val
+        
+        return data
+
+
+    def serialize_iter(self, obj):
+        """
+        Convert iterables into a serializable representation.
+        """
+        return [self.serialize(item) for item in obj]
+
+
+    def serialize_func(self, obj):
+        """
+        Convert no-arg methods and functions into a serializable representation.
+        """
+        return self.serialize(obj())
+
+
+    def serialize_manager(self, obj):
+        """
+        Convert a model manager into a serializable representation.
+        """
+        return self.serialize_iter(obj.all())
+
+
+    def serialize_fallback(self, obj):
+        """
+        Convert any unhandled object into a serializable representation.
+        """
+        return safeuni(obj)
+ 
+ 
+    def serialize(self, obj):
+        """
+        Convert any object into a serializable representation.
+        """
+        
+        if isinstance(obj, (dict, models.Base)):
+            # Model instances & dictionaries
+            return self.serialize_model(obj)
+        elif isinstance(obj, (tuple, list, set)): # What are query types in sqlalchemy?
+            # basic iterables
+            return self.serialize_iter(obj)
+        elif inspect.isfunction(obj) and not inspect.getargspec(obj)[0]:
+            # function with no args
+            return self.serialize_func(obj)
+        elif inspect.ismethod(obj) and len(inspect.getargspec(obj)[0]) <= 1:
+            # bound method
+            return self.serialize_func(obj)
+
+        # Protected types are passed through as is.
+        # (i.e. Primitives like None, numbers, dates, and Decimals.)
+        if isinstance(obj, (int, basestring, float)) or obj is None:
+            return obj
+
+        # All other values are converted to string.
+        return self.serialize_fallback(obj)
+
+
 class RestController (Controller):
     """
     Base controller for REST endpoints.
@@ -92,6 +362,9 @@ class RestController (Controller):
     - ``REST_DELETE
     
     """
+    
+    def get_serializer(self):
+        return Serializer()
     
     def get_access_rules(self):
         if hasattr(self, 'access_rules'):
@@ -126,6 +399,10 @@ class RestController (Controller):
                 if hasattr(self, verb):
                     method_handler = getattr(self, verb)
                     response_data = method_handler(*args, **kwargs)
+                    
+                    serializer = self.get_serializer()
+                    response_data = serializer.serialize_model(response_data)
+                    
                     return self.json(response_data)
             
             return self.no_method()
--- tests/integrationtests/rest_tests.py
+++ tests/integrationtests/rest_tests.py
@@ -11,7 +11,11 @@ from framework.config import Config
 from framework.session_holder import SessionHolder
 import main
 
-class NeedsResourcesTests (AppSetupMixin, TestCase):
+from controllers.rest import Serializer
+from controllers.rest import NeedInstance
+from controllers.rest import NotFoundError
+
+class Test_Needs_REST_endpoint (AppSetupMixin, TestCase):
     fixtures = ['test_data.sql']
     
     def test_AnonymousUserNotAllowedToCreateNeeds(self):
@@ -76,4 +80,46 @@ class NeedsResourcesTests (AppSetupMixin, TestCase):
 #            },
 #            status=200)
     
+class Test_NeedsRestEndpoint_GET (AppSetupMixin, TestCase):
+    fixtures = ['aarons_db_20110826.sql']
+    
+    @istest
+    def should_return_a_reasonable_representation_of_a_need(self):
+        # Check out http://webpy.org/cookbook/testing_with_paste_and_nose for
+        # more about testing with Paste.
+        
+        response = self.app.get('/rest/v1/needs/1/', status=200)
+        assert_in('"date": "2011-08-31"', response)
 
+
+class Test_NeedInstance_REST_READ (AppSetupMixin, TestCase):
+    fixtures = ['aarons_db_20110826.sql']
+    
+    @istest
+    def should_return_a_reasonable_string_representation_of_a_need(self):
+        controller = NeedInstance()
+        response = controller.REST_READ(1)
+        
+        assert isinstance(response, dict)
+        assert response
+    
+    @istest
+    def should_not_raise_NotFoundError(self):
+        controller = NeedInstance()
+        
+        try:
+            response = controller.REST_READ(1)
+        except NotFoundError:
+            ok_(False)
+    
+
+class Test_Serializer_serialize (AppSetupMixin, TestCase):
+    
+    @istest
+    def dates_are_converted_to_iso_strings(self):
+        import datetime
+        
+        serializer = Serializer()
+        serialized = serializer.serialize(datetime.date(2011,8,2))
+        
+        assert_equal(serialized, '2011-08-02')
