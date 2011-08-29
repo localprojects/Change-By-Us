b6d20a26b6ec831cae0a10fc151abc574441f2ad
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2011-08-29 13:02:04 -0700

    Use the models.sql file from tag 2.0.3 as the initial models file, as this appears to be what's installed on the staging server

--- giveaminute/migrations/versions/001_Initial_models.py
+++ giveaminute/migrations/versions/001_Initial_models.py
@@ -14,8 +14,38 @@ def upgrade(migrate_engine):
 #        sql = initial_file.read()
 #        migrate_engine.execute(sql)
 
+    meta = MetaData(migrate_engine)
+
+    # Load the project and user tables (for the foreign keys)
+    project = Table('project', meta, autoload=True)
+    user = Table('user', meta, autoload=True)
+
+    # Create the project place table
+    needs = Table('project_needs', meta,
+        Column('id', Integer, primary_key=True),
+        Column('type', String(10)),
+        Column('item_needed', String(64)),
+        Column('num_needed', Integer),
+        Column('description', Text),
+        Column('project_id', Integer, ForeignKey('project.project_id'), nullable=False),
+    )
+    needs.create()
+
+    volunteers = Table('project_need_volunteers', meta,
+        Column('need_id', Integer, ForeignKey('project_needs.id'), primary_key=True),
+        Column('member_id', Integer, ForeignKey('user.user_id'), primary_key=True),
+    )
+    volunteers.create()
+
 
 def downgrade(migrate_engine):
     # Operations to reverse the above upgrade go here.
 
-    pass
+    # Get rid of the created tables.
+    volunteers = Table('project_need_volunteers', meta, autoload=True)
+    volunteers.drop()
+
+    needs = Table('project_needs', meta, autoload=True)
+    needs.drop()
+
+    # The SQL schema isn't as important.  It'll get overwritten anyway.
