fb65fedbf60481d37e097ea9db290f53b84cae26
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2011-08-29 11:10:26 -0700

    Comment out the initial migration step by default (so that we're not inadvertently blowing peoples databases away

--- giveaminute/migrations/versions/001_Initial_models.py
+++ giveaminute/migrations/versions/001_Initial_models.py
@@ -4,14 +4,18 @@ from migrate import *
 def upgrade(migrate_engine):
     # Upgrade operations go here. Don't create your own engine; bind migrate_engine
     # to your metadata
-    
+
     import os
-    with open(os.path.join(os.path.dirname(__file__), '000_Initial_models.sql')) as initial_file:
-        sql = initial_file.read()
-        migrate_engine.execute(sql)
+    # Uncomment the following lines if you do not yet have a database to set up.
+    # If you run this migration, it will blow away the data currently contained
+    # in your database and start new.
+    #
+#    with open(os.path.join(os.path.dirname(__file__), '000_Initial_models.sql')) as initial_file:
+#        sql = initial_file.read()
+#        migrate_engine.execute(sql)
 
 
 def downgrade(migrate_engine):
     # Operations to reverse the above upgrade go here.
-    
+
     pass
