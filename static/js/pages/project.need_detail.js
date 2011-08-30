a821cf40d4527fa06dad6ab7ed1358386a4ceb43
Tyler Stalder <tyler@stalder.me>
2011-08-30 15:03:55 -0700

    add client side template for need detail

---
+++ static/js/pages/project.need_detail.js
@@ -0,0 +1,49 @@
+var tc = tc || {};
+tc.gam = tc.gam || {};
+tc.gam.project_widgets = tc.gam.project_widgets || {};
+
+tc.gam.project_widgets.need_detail = function(options) {
+    tc.util.log('project.need_detail');
+    var dom = options.dom;
+
+    var mergeTemplate = function(need_details) {
+        var new_details = tc.jQ.extend(true, {
+                day: function() { return this.date ? (new Date(this.date).getUTCDate()) : ''; },
+                month: function() { return this.date ? (new Date(this.date).getUTCMonth()+1) : ''; },
+                avatar: function() {return this.avatar_path ? ('/static/images/' + this.avatar_path) : '/static/images/thumb_genAvatar.jpg'; }
+            }, need_details),
+            $html = ich.need_detail_tmpl(new_details);
+            
+        dom.find('.need-detail-cont').html($html);
+    };
+
+    var initDetailTmpl = function(need_id, callback) {
+      tc.gam.project_data.getNeedDetails(need_id, function(data){
+        mergeTemplate(data);
+        if (callback) {
+          callback();
+        }
+      });
+    };
+
+    /**
+     * Function: bindEvents
+     * Bind events for this widget
+     */
+    var bindEvents = function() {
+        tc.jQ(tc).bind('show-project-widget', function(event, widgetName, id) {
+            if (options.name === widgetName) {
+                tc.util.log('&&& showing ' + options.name);
+                initDetailTmpl(id, function(){
+                    dom.show();
+                });
+                
+            } else {
+                tc.util.log('&&& hiding ' + options.name);
+                dom.hide();
+            }
+        });
+    };
+
+    bindEvents();
+};
--- static/js/tc.gam.project.js
+++ static/js/tc.gam.project.js
@@ -16,7 +16,10 @@ tc.gam.project = function(app, dom) {
         'needs': tc.gam.project_widgets.needs(
             tc.jQ.extend({ name: 'needs', dom: dom.find('.project-section.needs') }, widget_options)
         ),
-        'need-form': tc.gam.project_widgets.need_form(
+        'need-detail': tc.gam.project_widgets.need_detail(
+            tc.jQ.extend({ name: 'need-detail', dom: dom.find('.project-section.need-detail') }, widget_options)
+        ),
+       'need-form': tc.gam.project_widgets.need_form(
             tc.jQ.extend({ name: 'need-form', dom: dom.find('.project-section.need-form') }, widget_options)
         ),
         'infopane': tc.gam.project_widgets.infopane(
