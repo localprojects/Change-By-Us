9066d17f02edc13e98f899c00f69a820a33a8693
Alan Palazzolo <github@zzolo.org>
2011-09-13 15:54:46 -0700

    add test for creating users as unauthenticated user

---
+++ tests/integrationtests/admin.py
@@ -0,0 +1,37 @@
+import json
+from unittest2 import TestCase
+from paste.fixture import TestApp
+from nose.tools import *
+from lib import web
+from mock import Mock
+
+import mixins
+from   mixins import AppSetupMixin
+
+from framework.config import Config
+from framework.session_holder import SessionHolder
+import main
+
+class Test_anonymous_user_admin (AppSetupMixin, TestCase):
+    fixtures = ['aarons_db_20110826.sql']
+
+    @istest
+    def should_not_allow_anonymous_user_to_create_user(self):
+        # Check out http://webpy.org/cookbook/testing_with_paste_and_nose for
+        # more about testing with Paste.
+        response = self.app.post('/admin/user/add',
+            params={
+                'f_name': 'John',
+                'l_name': 'Smith',
+                'email': 'jsmith@example.com',
+                'password': 'password',
+                'role': '1',
+                'affiliation': '',
+            },
+            status=303)
+        db = main.sessionDB()
+        
+        # Check to see if the user was created even though the response
+        # returned a redirect (303).
+        results = db.query("select user_id from user where email = 'jsmith@example.com'")
+        assert_equal(len(results),0)
