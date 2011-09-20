f42ba1bebb0e7f92222d8a66f94e2550b4dde9e1
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2011-09-20 14:48:10 -0700

    Add a neattime custom filter to give me pretty times.

--- helpers/custom_filters.py
+++ helpers/custom_filters.py
@@ -17,9 +17,31 @@ def json_filter(data):
 def datetimeformat(value, format='%H:%M / %d-%m-%Y'):
     return value.strftime(format)
 
+def neat_time(dt):
+    """Return the time in dt as a neat string.
+
+    Examples:
+
+    >>> neat_time(time(7, 30))
+    7:30AM
+    >>> neat_time(time(14, 00))
+    2PM
+
+    """
+    if dt.minute:
+        timestring = dt.strftime('%I:%M%p')
+    else:
+        timestring = dt.strftime('%I%p')
+
+    if timestring[0] == '0':
+        timestring = timestring[1:]
+
+    return timestring
+
 filters = {
     'strslice': strslice,
     'urlencode': urlencode,
     'json': json_filter,
     'datetime': datetimeformat,
+    'neattime': neat_time,
 }
