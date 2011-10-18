e437b20cbd7ebf1a7d58f59466cb2541fb9f62f8
Aaron Ogle <atogle@gmail.com>
2011-10-17 17:08:24 -0700

    add html/css for inkind form

--- static/js/pages/project.inkind_form.js
+++ static/js/pages/project.inkind_form.js
@@ -8,10 +8,40 @@ tc.gam.project_widgets.inkind_form = function(options) {
       $window = tc.jQ(window),
       self = {};
 
+  var initForm = function(need_id, callback) {
+    var project_id = options.app.app_page.data.project.project_id;
+
+    dom.find('input[type=radio]').prettyCheckboxes();
+
+    // tc.gam.project_data.getProjectEvents(project_id, function(events) {
+    //   cached_events = events;
+    // 
+    //   if (need_id) {
+    //   //We are editing an existing need
+    //       tc.gam.project_data.getNeedDetails(need_id, function(data){
+    //           mergeTemplate(data);
+    //           if (callback) {
+    //               callback();
+    //           }
+    //           initMerlin(need_id);
+    //       });
+    //   } else {
+    //       mergeTemplate();
+          if (callback) {
+              callback();
+          }
+    //       initMerlin();
+    //   }
+    // });
+  };
+      
   tc.jQ(tc).bind('show-project-widget', function(event, widgetName) {
     if (options.name === widgetName) {
       tc.util.log('&&& showing ' + options.name);
-      dom.show();
+
+      initForm(id, function(){
+          dom.show();
+      });
 
       if((dom.offset().top - $window.scrollTop()) < 0){
         $window.scrollTop(0);
