8a816fede7603a1f43cf174dfac5aa3fc24792be
Sundar Raman <sundar@localprojects.net>
2011-10-19 10:16:38 -0400

    Cleans up management of minifier and build process

--- fabfile.py
+++ fabfile.py
@@ -86,7 +86,6 @@ env.clean_build = False
 # SSH Key configuration
 env.key_filename = os.path.expanduser(env.key_filename)
 env.ssh_port = 48022
-env.run_minifier = True
 
 # We need to make the hosts into a list
 env.hosts = env.hosts.split(',')
@@ -144,6 +143,10 @@ env.protected_folders = []
 # template file, but in addition to the etc_path folder. SO DO NOT ADD
 # etc_path to this list!
 env.template_paths = []
+
+# Should we minify?
+env.run_minifier = True
+env.minifier_cmd = 'python %(build_path)s/scripts/minifier/minifier.py -v -c %(build_path)s/scripts/minifier.conf --force'
 
 #------------------------------------------------
 # AWS related tasks require some configurations
@@ -326,7 +329,8 @@ def create_config_files():
         raise Exception("%(rcfile)s does not exist. See rcfile.sample and run fab --config=rcfile.name <commands>!" % env)
 
     # Make sure that the code is the latest in the build_path
-    local('cd %(build_path)s && git fetch origin && git pull origin && git checkout %(branch)s' % env)
+    with lcd(env.build_path):
+        local('git fetch origin && git pull origin && git checkout %(branch)s' % env)
     for item in env.config_files:
         if not os.path.exists(item.get('local_config_template')):
             raise Exception("Unable to find configuration template file (%s) to create config from" % item.get('local_config_template'))
@@ -605,31 +609,36 @@ def bundle_code():
             "Create an archive from the current Git master branch and upload it"
             local('git clone --depth 0 %(repository)s %(build_path)s' % env)
         else:
-            local('cd %(build_path)s && if [ $(git config --get remote.origin.url) != "%(repository)s" ];then echo "Existing repository is not the one requested. Deleting build path."; rm -rf %(build_path)s; fi' % env)
+            with lcd(env.build_path):
+                local('if [ $(git config --get remote.origin.url) != "%(repository)s" ];then echo "Existing repository is not the one requested. Deleting build path."; rm -rf %(build_path)s; fi' % env)
             
         try:
-            local('cd %(build_path)s && git clean -d -x -f' % env)
+            with lcd(env.build_path):
+                local('git clean -d -x -f' % env)
         except:
             local('rm -rf %(build_path)s' % env)
             "Create an archive from the current Git master branch and upload it"
             local('git clone --depth 0 %(repository)s %(build_path)s' % env)
             
-        local('cd %(build_path)s && git clean -d -x -f && git pull origin && git checkout %(branch)s' % env)
-        local('cd %(build_path)s && git submodule init && git submodule update' % env)
-        env.release = local('cd %(build_path)s && git rev-parse %(branch)s | cut -c 1-9' % env, capture=True)
-        # Save the revision information to a file for post-deployment info
-        local('cd %(build_path)s && git rev-parse %(branch)s > REVISION.txt' % env)
-        # To be safe, remove any newline characters
-        env.release = re.sub('[\r\n]', '', env.release)
-        # Archive the bundle for upload
-        local('cd %(build_path)s && git archive --format=tar %(branch)s > %(build_path)s/%(release)s.tar' % env)
+        with lcd(env.build_path):
+            local('git clean -d -x -f && git pull origin && git checkout %(branch)s' % env)
+            local('git submodule init && git submodule update' % env)
+            env.release = local('git rev-parse %(branch)s | cut -c 1-9' % env, capture=True)
+            # Save the revision information to a file for post-deployment info
+            local('git rev-parse %(branch)s > REVISION.txt' % env)
+            # To be safe, remove any newline characters
+            env.release = re.sub('[\r\n]', '', env.release)
+            # Archive the bundle for upload
+            local('git archive --format=tar %(branch)s > %(build_path)s/%(release)s.tar' % env)
 
-        if env.run_minifier == True and os.path.exists(os.path.join(env.build_path, 'scripts', 'minifier', 'minifier.py')):
-            local('cd %(build_path)s && python scripts/minifier/minifier.py -v -c scripts/minifier.conf --force' % env)
-            local('cd %(build_path)s && git status -s | grep -i "^ M" | tr -s " " | cut -d\  -f 3 | xargs tar -v --append --file=%(release)s.tar ' % env)
+            if env.run_minifier == True and env.get('minifier_cmd') is not None: 
+                with settings(warn_only=True):
+                    # If minification fails, we only want to warn about it, not crash
+                    local(env.minifier_cmd) # 'python %(build_path)s/scripts/minifier/minifier.py -v -c %(build_path)s/scripts/minifier.conf --force' % env)
+                    local('git status -s | grep -i "^ M" | tr -s " " | cut -d\  -f 3 | xargs tar -v --append --file=%(release)s.tar ' % env)
 
-        # Add any updated files
-        local('cd %(build_path)s && tar --append --file=%(release)s.tar REVISION.txt' % env)
+            # Add any updated files
+            local('tar --append --file=%(release)s.tar REVISION.txt' % env)
     elif env.scm == "git-svn":
         # Get repo information and store it to REVISION.txt
         local('cd %(repository)s && git svn info > %(build_path)s/REVISION.txt' % env)
