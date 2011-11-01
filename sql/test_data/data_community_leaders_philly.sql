67ebd982b0058a487bc3a5ab17ffa06dc4af0006
Alan Palazzolo <alan@codeforamerica.org>
2011-11-01 16:49:19 -0700

    Added more leaders to Philly.

--- sql/test_data/data_community_leaders_philly.sql
+++ sql/test_data/data_community_leaders_philly.sql
@@ -6,3 +6,5 @@ INSERT INTO `community_leader` (`id`, `display_name`, `title`, `image_path`, `or
 INSERT INTO `community_leader` (`id`, `display_name`, `title`, `image_path`, `order`) VALUES(2, 'Charles Ramsey', 'Commissioner, Philadelphia Police Department', 'images/leaders/philly/pc-ramsey.jpg', 2);
 INSERT INTO `community_leader` (`id`, `display_name`, `title`, `image_path`, `order`) VALUES(3, 'Michael Nutter', 'Mayor, City of Philadelphia', 'images/leaders/philly/mayor-nutter.jpg', 3);
 INSERT INTO `community_leader` (`id`, `display_name`, `title`, `image_path`, `order`) VALUES(4, 'Lori Shorr', 'Chief Education Officer, City of Philadelphia', 'images/leaders/philly/lori-shorr.jpg', 4);
+INSERT INTO `community_leader` (`id`, `display_name`, `title`, `image_path`, `order`) VALUES(5, 'Catherine Wolfgang', 'Chief Service Officer, Mayors Office of Civic Engagement and Volunteer Service, City of Philadelphia', 'images/leaders/philly/catherine-wolfgang.jpg', 5);
+INSERT INTO `community_leader` (`id`, `display_name`, `title`, `image_path`, `order`) VALUES(6, 'Claire Robertson-Craft', 'Board Chair for Young Involved Philadelphia', 'images/leaders/philly/claire-robertson-craft.jpg', 6);
