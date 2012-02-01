e396b5e7a84b5f39fb3d98b9230672521bc741d2
Sundar Raman <sundar@localprojects.net>
2012-02-01 11:03:06 -0500

    Optimize keyword search

--- giveaminute/keywords.py
+++ giveaminute/keywords.py
@@ -2,16 +2,21 @@
     :copyright: (c) 2011 Local Projects, all rights reserved
     :license: Affero GNU GPL v3, see LICENSE for more details.
 """
+from framework.controller import log
 
 # find keywords in a string
 def getKeywords(db, s):
-    sql = "select keyword from keyword"
-    data = list(db.query(sql))
+    """Get all matches for passed in string in keyword tables
     
+    :param db: database handle
+    :param s:  string to look for
+    :returns   list of matching keywords
+    """
     words = []
-    
-    for d in data:
-        if (d.keyword in s):
-            words.append(d.keyword)
-            
+    if isinstance(s, str): s = [s]
+    if not isinstance(s, list):
+        log.error("getKeywords requested for a non-string, non-list value: %s. Cannot process!" % s)
+    else:
+        words = list(db.query("select keyword from keyword where keyword in $lookfor", vars=dict(lookfor=s)))
+
     return words
