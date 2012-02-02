dfa4f198bc596e277e048a841af778362b98ee33
Sundar Raman <cybertoast@gmail.com>
2012-02-02 09:55:02 -0500

    Add unicode into the check

--- giveaminute/keywords.py
+++ giveaminute/keywords.py
@@ -3,6 +3,7 @@
     :license: Affero GNU GPL v3, see LICENSE for more details.
 """
 from framework.controller import log
+import re
 
 # find keywords in a string
 def getKeywords(db, s):
@@ -13,10 +14,12 @@ def getKeywords(db, s):
     :returns   list of matching keywords
     """
     words = []
-    if isinstance(s, str): s = [s]
+    if isinstance(s, str) or isinstance(s, unicode): s = re.split(r'[,\s+]', s)
     if not isinstance(s, list):
         log.error("getKeywords requested for a non-string, non-list value: %s. Cannot process!" % s)
     else:
         words = list(db.query("select keyword from keyword where keyword in $lookfor", vars=dict(lookfor=s)))
 
     return words
+
+
